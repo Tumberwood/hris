c27164e7a3adedbd6b53c5cd9caf93d479ed6e18
vincmeister <danny.harjono@gmail.com>
2023-07-18 11:29:29 +0700

    + cancel approve

--- usersc/applications/models/htpxxth/htpxxth_post_approve.php
+++ usersc/applications/models/htpxxth/htpxxth_post_approve.php
@@ -15,52 +15,64 @@
 	$state = $_POST['state'];
 
 	if($state == 1){
-		$is_approve = 1;
-	}else{
-		$is_approve = -9;
+
+		$qs_htpxxth = $db
+			->query('select', 'htpxxth' )
+			->get([
+				'htpxxth.id as id_transaksi',
+				'htpxxth.id_hemxxmh as id_hemxxmh',
+				'htpxxth.id_htpxxmh as id_htpxxmh',
+				'htpxxth.kode as kode',
+				'htpxxth.tanggal as tanggal',
+				'htpxxth.keterangan as keterangan',
+				'htpxxth.jam_awal as jam_awal',
+				'htpxxth.jam_akhir as jam_akhir',
+				'htpxxmh.kode as htpxxmh_kode',
+				'htlgrmh.id as id_htlgrmh',
+				'htlgrmh.kode as htlgrmh_kode'
+			] )
+			->join('hemxxmh','hemxxmh.id = htpxxth.id_hemxxmh','LEFT' )
+			->join('htpxxmh','htpxxmh.id = htpxxth.id_htpxxmh','LEFT' )
+			->join('htlgrmh','htlgrmh.id = htpxxmh.id_htlgrmh','LEFT' )
+			->where('htpxxth.id', $id_htpxxth )
+			->exec();
+
+		$rs_htpxxth = $qs_htpxxth->fetch();
+		
+		// $tanggal  = Carbon::parse($rs_htpxxth['tanggal']);
+			
+		$qi = $db
+			->query('insert', 'htlxxrh')
+			->set('id_transaksi', $rs_htpxxth['id_transaksi'] )
+			->set('id_htlgrmh', $rs_htpxxth['id_htlgrmh'] )
+			->set('id_htlxxmh', $rs_htpxxth['id_htpxxmh'] )
+			->set('id_hemxxmh', $rs_htpxxth['id_hemxxmh'] )
+			->set('kode', $rs_htpxxth['kode'] )
+			->set('is_approve', $state )
+			// ->set('tanggal', $tanggal->format('Y-m-d') )
+			->set('tanggal', $rs_htpxxth['tanggal'] )
+			->set('keterangan', $rs_htpxxth['keterangan'] )
+			->set('jenis', 2 )
+			->set('htlxxmh_kode', $rs_htpxxth['htpxxmh_kode'] )
+			->set('htlgrmh_kode', $rs_htpxxth['htlgrmh_kode'] )
+			->set('jumlah', 1 )
+			->set('jam_awal', $rs_htpxxth['jam_awal'] )
+			->set('jam_akhir', $rs_htpxxth['jam_akhir'] )
+			->exec();
+	}elseif($state == 2){
+		$qd_htlxxrh = $db
+			->query('delete', 'htlxxrh')
+			->where('id_transaksi', $id_htpxxth )
+			->exec();
+
+	}elseif($state == -9){
+		$qu_htlxxrh = $db
+			->query('update', 'htlxxrh')
+			->set('is_approve', $state)
+			->where('id_transaksi', $id_htpxxth )
+			->exec();
 	}
 
-	$qs_htpxxth = $db
-		->query('select', 'htpxxth' )
-		->get([
-			'htpxxth.id as id_transaksi',
-			'htpxxth.id_hemxxmh as id_hemxxmh',
-			'htpxxth.id_htpxxmh as id_htpxxmh',
-			'htpxxth.kode as kode',
-			'htpxxth.tanggal as tanggal',
-			'htpxxth.keterangan as keterangan',
-			'htpxxth.jam_awal as jam_awal',
-			'htpxxth.jam_akhir as jam_akhir',
-			'htpxxmh.kode as htpxxmh_kode',
-            'htlgrmh.id as id_htlgrmh',
-            'htlgrmh.kode as htlgrmh_kode'
-		] )
-		->join('hemxxmh','hemxxmh.id = htpxxth.id_hemxxmh','LEFT' )
-		->join('htpxxmh','htpxxmh.id = htpxxth.id_htpxxmh','LEFT' )
-		->join('htlgrmh','htlgrmh.id = htpxxmh.id_htlgrmh','LEFT' )
-		->where('htpxxth.id', $id_htpxxth )
-		->exec();
-
-	$rs_htpxxth = $qs_htpxxth->fetch();
 	
-	$tanggal  = Carbon::parse($rs_htpxxth['tanggal']);
-		
-    $qi = $db
-        ->query('insert', 'htlxxrh')
-        ->set('id_transaksi', $rs_htpxxth['id_transaksi'] )
-        ->set('id_htlgrmh', $rs_htpxxth['id_htlgrmh'] )
-		->set('id_htlxxmh', $rs_htpxxth['id_htpxxmh'] )
-        ->set('id_hemxxmh', $rs_htpxxth['id_hemxxmh'] )
-        ->set('kode', $rs_htpxxth['kode'] )
-        ->set('is_approve', $is_approve )
-        ->set('tanggal', $tanggal->format('Y-m-d') )
-        ->set('keterangan', $rs_htpxxth['keterangan'] )
-        ->set('jenis', 2 )
-        ->set('htlxxmh_kode', $rs_htpxxth['htpxxmh_kode'] )
-        ->set('htlgrmh_kode', $rs_htpxxth['htlgrmh_kode'] )
-        ->set('jumlah', 1 )
-        ->set('jam_awal', $rs_htpxxth['jam_awal'] )
-        ->set('jam_akhir', $rs_htpxxth['jam_akhir'] )
-        ->exec();
 
 ?>
