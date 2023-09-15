83a1d97a7337333d2a51e716b21caa3292ea01a2
Ferry Ibnu <ferryibnu86@gmail.com>
2023-09-15 12:54:55 +0700

    f cancel approve replacement

--- usersc/applications/models/htsrptd/htsrptd_post_approve.php
+++ usersc/applications/models/htsrptd/htsrptd_post_approve.php
@@ -141,6 +141,227 @@
                 // END insert pengganti tambahan
             }
             
+        } else if($state == 2) {
+            //CANCEL APPROVE
+            $qs_htsrptd = $db
+                ->query('select', 'htsrptd' )
+                ->get([
+                    'htsrptd.id_hemxxmh_pengaju as id_hemxxmh_pengaju',
+                    'htsrptd.id_hemxxmh_pengganti as id_hemxxmh_pengganti',
+                    'htsrptd.id_htsxxmh_pengaju as id_htsxxmh_pengaju',
+                    'htsrptd.id_htsxxmh_pengganti as id_htsxxmh_pengganti',
+                    'htsrptd.kode as kode',
+                    'htsrptd.tanggal as tanggal',
+                    'htsrptd.keterangan as keterangan'
+                ] )
+                ->where('htsrptd.id', $id_transaksi_h )
+                ->exec();
+
+            $rs_htsrptd = $qs_htsrptd->fetch();
+            $keterangan = $rs_htsrptd['keterangan'];
+            // BEGIN non aktif
+            // harusnya bisa pakai where or, tapi belum berhasil
+            $qu_htssctd_pengaju = $db
+                ->query('update', 'htssctd')
+                ->set('is_active',0)
+                ->set('keterangan', $keterangan)
+                ->where('tanggal', $rs_htsrptd['tanggal'])
+                ->where('id_hemxxmh', $rs_htsrptd['id_hemxxmh_pengaju'])
+                ->exec();
+
+            $qu_htssctd_pengganti = $db
+                ->query('update', 'htssctd')
+                ->set('is_active',0)
+                ->set('keterangan', $keterangan)
+                ->where('tanggal', $rs_htsrptd['tanggal'])
+                ->where('id_hemxxmh', $rs_htsrptd['id_hemxxmh_pengganti'])
+                ->exec();
+            // END non aktif
+                
+            // Begin insert pengaju
+            $qr_htssctd_pengaju = $db
+                ->raw()
+                ->bind(':id_htsxxmh_pengaju', $rs_htsrptd['id_htsxxmh_pengaju'])
+                ->bind(':tanggal_pengaju', $rs_htsrptd["tanggal"])
+                ->exec('
+                    INSERT INTO htssctd
+                    (
+                        id_hemxxmh,
+                        id_htsxxmh,
+                        keterangan,
+                        tanggal,
+                        jam_awal,
+                        jam_akhir,
+                        jam_awal_istirahat,
+                        jam_akhir_istirahat,
+                        menit_toleransi_awal_in,
+                        menit_toleransi_akhir_in,
+                        menit_toleransi_awal_out,
+                        menit_toleransi_akhir_out,
+
+                        tanggaljam_awal_t1,
+                        tanggaljam_awal,
+                        tanggaljam_awal_t2,
+                        tanggaljam_akhir_t1,
+                        tanggaljam_akhir,
+                        tanggaljam_akhir_t2,
+                        tanggaljam_awal_istirahat,
+                        tanggaljam_akhir_istirahat
+                    )
+                    SELECT
+                        '.$rs_htsrptd["id_hemxxmh_pengaju"].',
+                        '.$rs_htsrptd["id_htsxxmh_pengaju"].',
+                        "'.$keterangan.'",
+                        "'.$rs_htsrptd["tanggal"].'",
+                        htsxxmh.jam_awal,
+                        htsxxmh.jam_akhir,
+                        htsxxmh.jam_awal_istirahat,
+                        htsxxmh.jam_akhir_istirahat,
+                        htsxxmh.menit_toleransi_awal_in,
+                        htsxxmh.menit_toleransi_akhir_in,
+                        htsxxmh.menit_toleransi_awal_out,
+                        htsxxmh.menit_toleransi_akhir_out,
+                        CONCAT(:tanggal_pengaju, " ", TIME(DATE_SUB(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_awal_in MINUTE))) AS tanggaljam_awal_t1,
+                        CONCAT(:tanggal_pengaju, " ", htsxxmh.jam_awal) AS tanggaljam_awal,
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN DATE_ADD(:tanggal_pengaju, INTERVAL 1 DAY)
+                                ELSE :tanggal_pengaju
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_awal_t2,
+
+                        CASE
+                            WHEN htsxxmh.kode like "malam%" AND htsxxmh.jam_akhir <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(:tanggal_pengaju, INTERVAL 1 DAY), " ", TIME(DATE_SUB(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE)))
+                            ELSE CONCAT(:tanggal_pengaju, " ", TIME(DATE_SUB(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE)))
+                        END AS tanggaljam_akhir_t1,
+                        CASE
+                            WHEN htsxxmh.kode like "malam%" AND htsxxmh.jam_akhir <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(:tanggal_pengaju, INTERVAL 1 DAY), " ", htsxxmh.jam_akhir)
+                            ELSE CONCAT(:tanggal_pengaju, " ", htsxxmh.jam_akhir)
+                        END AS tanggaljam_akhir,
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE) >= "24:00:00" 
+                                    OR htsxxmh.kode like "malam%" AND htsxxmh.jam_akhir <= "12:00:00" THEN DATE_ADD(:tanggal_pengaju, INTERVAL 1 DAY)
+                                ELSE :tanggal_pengaju
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_akhir_t2,
+                        CONCAT(:tanggal_pengaju, " ", htsxxmh.jam_awal_istirahat) AS tanggaljam_awal_istirahat,
+                        CONCAT(:tanggal_pengaju, " ", htsxxmh.jam_akhir_istirahat) AS tanggaljam_akhir_istirahat
+                    FROM htsxxmh
+                    WHERE 
+                        id = :id_htsxxmh_pengaju
+                ');
+            // END insert pengaju
+
+            // BEGIN insert pengganti
+            $qr_htssctd_pengganti = $db
+                ->raw()
+                ->bind(':id_htsxxmh_pengganti', $rs_htsrptd['id_htsxxmh_pengganti'])
+                ->bind(':tanggal_pengganti', $rs_htsrptd["tanggal"])
+                ->exec('
+                    INSERT INTO htssctd
+                    (
+                        id_hemxxmh,
+                        id_htsxxmh,
+                        keterangan,
+                        tanggal,
+                        jam_awal,
+                        jam_akhir,
+                        jam_awal_istirahat,
+                        jam_akhir_istirahat,
+                        menit_toleransi_awal_in,
+                        menit_toleransi_akhir_in,
+                        menit_toleransi_awal_out,
+                        menit_toleransi_akhir_out,
+
+                        tanggaljam_awal_t1,
+                        tanggaljam_awal,
+                        tanggaljam_awal_t2,
+                        tanggaljam_akhir_t1,
+                        tanggaljam_akhir,
+                        tanggaljam_akhir_t2,
+                        tanggaljam_awal_istirahat,
+                        tanggaljam_akhir_istirahat
+                    )
+                    SELECT
+                        '.$rs_htsrptd["id_hemxxmh_pengganti"].',
+                        '.$rs_htsrptd["id_htsxxmh_pengganti"].',
+                        "'.$keterangan.'",
+                        :tanggal_pengganti,
+                        htsxxmh.jam_awal,
+                        htsxxmh.jam_akhir,
+                        htsxxmh.jam_awal_istirahat,
+                        htsxxmh.jam_akhir_istirahat,
+                        htsxxmh.menit_toleransi_awal_in,
+                        htsxxmh.menit_toleransi_akhir_in,
+                        htsxxmh.menit_toleransi_awal_out,
+                        htsxxmh.menit_toleransi_akhir_out,
+
+                        CONCAT(:tanggal_pengganti, " ", TIME(DATE_SUB(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_awal_in MINUTE))) AS tanggaljam_awal_t1,
+                        CONCAT(:tanggal_pengganti, " ", htsxxmh.jam_awal) AS tanggaljam_awal,
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN DATE_ADD(:tanggal_pengganti, INTERVAL 1 DAY)
+                                ELSE :tanggal_pengganti
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_awal_t2,
+                        CONCAT(:tanggal_pengganti, " ", TIME(DATE_SUB(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_awal_out MINUTE))) AS tanggaljam_akhir_t1,
+                        CONCAT(:tanggal_pengganti, " ", htsxxmh.jam_akhir) AS tanggaljam_akhir,
+                        
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE) >= "24:00:00" 
+                                    OR htsxxmh.kode like "malam%" AND htsxxmh.jam_akhir <= "12:00:00" THEN DATE_ADD(:tanggal_pengganti, INTERVAL 1 DAY)
+                                ELSE :tanggal_pengganti
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_akhir_t2,
+                        CONCAT(:tanggal_pengganti, " ", htsxxmh.jam_awal_istirahat) AS tanggaljam_awal_istirahat,
+                        CONCAT(:tanggal_pengganti, " ", htsxxmh.jam_akhir_istirahat) AS tanggaljam_akhir_istirahat
+                    FROM htsxxmh
+                    WHERE 
+                        id = :id_htsxxmh_pengganti
+                ');
+
+        
+            // END insert pengganti
         }
     
         $db->commit();
