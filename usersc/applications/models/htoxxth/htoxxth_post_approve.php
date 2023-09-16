e596c5d4224fc0ecad4e25734456691cdbb72507
Ferry Ibnu <ferryibnu86@gmail.com>
2023-09-16 12:08:47 +0700

    f overtime post approve

--- usersc/applications/models/htoxxth/htoxxth_post_approve.php
+++ usersc/applications/models/htoxxth/htoxxth_post_approve.php
@@ -21,37 +21,36 @@
 
     try{
         $db->transaction();
-
+        // BEGIN ambil data htoemtd
+        $qs_htoemtd = $db
+            ->raw()
+            ->bind(':id_transaksi_h', $id_transaksi_h)
+            ->exec('SELECT
+                        htoemtd.id_hemxxmh AS id_hemxxmh,
+                        htoxxth.id AS id_htoxxth,
+                        htoemtd.id AS id_htoemtd,
+                        htoemtd.id_htotpmh AS id_htotpmh,
+                        htoxxth.id_heyxxmh AS id_heyxxmh,
+                        htoxxth.kode AS kode,
+                        htoxxth.tanggal AS tanggal,
+                        htoxxth.is_approve AS is_approve,
+                        htoemtd.is_istirahat AS is_istirahat,
+                        htoemtd.jam_awal AS jam_awal,
+                        htoemtd.jam_akhir AS jam_akhir,
+                        htoemtd.durasi_lembur_jam AS durasi_lembur_jam,
+                        htoemtd.durasi_lembur_menit AS durasi_lembur_menit,
+                        hemjbmh.id_hesxxmh AS id_hesxxmh
+                    FROM htoemtd
+                    LEFT JOIN htoxxth ON htoxxth.id = htoemtd.id_htoxxth
+                    LEFT JOIN hemjbmh ON hemjbmh.id_hemxxmh = htoemtd.id_hemxxmh
+                    WHERE htoemtd.is_active = 1
+                        AND htoxxth.is_active = 1
+                        AND htoxxth.id = :id_transaksi_h;
+                    '
+                    );
+        $rs_htoemtd = $qs_htoemtd->fetchAll();
+        // END ambil data htoemtd
         if($state == 1){
-            // BEGIN ambil data htoemtd
-            $qs_htoemtd = $db
-                ->raw()
-                ->bind(':id_transaksi_h', $id_transaksi_h)
-                ->exec('SELECT
-                            htoemtd.id_hemxxmh AS id_hemxxmh,
-                            htoxxth.id AS id_htoxxth,
-                            htoemtd.id AS id_htoemtd,
-                            htoemtd.id_htotpmh AS id_htotpmh,
-                            htoxxth.id_heyxxmh AS id_heyxxmh,
-                            htoxxth.kode AS kode,
-                            htoxxth.tanggal AS tanggal,
-                            htoxxth.is_approve AS is_approve,
-                            htoemtd.is_istirahat AS is_istirahat,
-                            htoemtd.jam_awal AS jam_awal,
-                            htoemtd.jam_akhir AS jam_akhir,
-                            htoemtd.durasi_lembur_jam AS durasi_lembur_jam,
-                            htoemtd.durasi_lembur_menit AS durasi_lembur_menit,
-                            hemjbmh.id_hesxxmh AS id_hesxxmh
-                        FROM htoemtd
-                        LEFT JOIN htoxxth ON htoxxth.id = htoemtd.id_htoxxth
-                        LEFT JOIN hemjbmh ON hemjbmh.id_hemxxmh = htoemtd.id_hemxxmh
-                        WHERE htoemtd.is_active = 1
-                            AND htoxxth.is_active = 1
-                            AND htoxxth.id = :id_transaksi_h;
-                        '
-                        );
-            $rs_htoemtd = $qs_htoemtd->fetchAll();
-            // END ambil data htoemtd
 
             foreach ($rs_htoemtd as $row_htoemtd) {
                 $durasi_lembur_menit = $row_htoemtd['durasi_lembur_jam'] * 60;
@@ -73,6 +72,7 @@
                     ->set( 'is_istirahat', $row_htoemtd['is_istirahat'] )
                     ->exec();
 
+                //JIKA BUKAN TI MAKA UPDATE JADWAL AWAL TI DAN AKHIR T2
                     $qs_hasil_awal_akhir = $db
                         ->raw()
                         ->bind(':id_hemxxmh', $row_htoemtd['id_hemxxmh'])
@@ -86,8 +86,8 @@
                                             WHEN (TIME(:jam_akhir) >= "19:00:00" AND TIME(:jam_akhir) <= "23:59:59")
                                                 OR (TIME(:jam_akhir) >= "00:00:00" AND TIME(:jam_akhir) <= "12:00:00" AND b.kode LIKE "malam%"
                                                 OR :jam_awal > :jam_akhir )
-                                            THEN DATE_ADD(CONCAT(DATE_ADD(a.tanggal, INTERVAL 1 DAY), " ", TIME(:jam_akhir)), INTERVAL 2 HOUR)
-                                            ELSE DATE_ADD(CONCAT(a.tanggal, " ", :jam_akhir), INTERVAL 2 HOUR)
+                                            THEN DATE_ADD(CONCAT(DATE_ADD(a.tanggal, INTERVAL 1 DAY), " ", TIME(:jam_akhir)), INTERVAL 1 HOUR)
+                                            ELSE DATE_ADD(CONCAT(a.tanggal, " ", :jam_akhir), INTERVAL 1 HOUR)
                                         END,
                                         "%Y-%m-%d %H:%i:%s"
                                     ) AS tanggaljam_akhir_t2
@@ -100,7 +100,28 @@
                                 );
                     $rs_hasil_awal_akhir = $qs_hasil_awal_akhir->fetch();
 
-                    //Update tanggaljam_awal_t1 dan tanggaljam_akhir_t2 ke Jadwal
+                //JIKA LEMBUR AWAL DAN ISTIRAHAT BUKAN TI MAKA UPDATE AWAL T1
+                if ($row_htoemtd['id_htotpmh'] == 1 && $row_htoemtd['is_istirahat'] != 2) {
+                    $qu_jadwal = $db
+                        ->query('update', 'htssctd')
+                        ->set('tanggaljam_awal_t1',$rs_hasil_awal_akhir['tanggaljam_awal_t1'])
+                        ->where('id_hemxxmh',$row_htoemtd['id_hemxxmh'])
+                        ->where('tanggal',$row_htoemtd['tanggal'])
+                    ->exec();
+                }
+
+                //JIKA LEMBUR AKHIR DAN ISTIRAHAT BUKAN TI MAKA UPDATE AKHIR T2
+                if ($row_htoemtd['id_htotpmh'] == 2 && $row_htoemtd['is_istirahat'] != 2) {
+                    $qu_jadwal = $db
+                        ->query('update', 'htssctd')
+                        ->set('tanggaljam_akhir_t2',$rs_hasil_awal_akhir['tanggaljam_akhir_t2'])
+                        ->where('id_hemxxmh',$row_htoemtd['id_hemxxmh'])
+                        ->where('tanggal',$row_htoemtd['tanggal'])
+                    ->exec();
+                }
+
+                //JIKA LEMBUR HARI LIBUR DAN ISTIRAHAT BUKAN TI MAKA UDATE KEDUANYA
+                if ($row_htoemtd['id_htotpmh'] == 4 && $row_htoemtd['is_istirahat'] != 2) {
                     $qu_jadwal = $db
                         ->query('update', 'htssctd')
                         ->set('tanggaljam_awal_t1',$rs_hasil_awal_akhir['tanggaljam_awal_t1'])
@@ -108,6 +129,7 @@
                         ->where('id_hemxxmh',$row_htoemtd['id_hemxxmh'])
                         ->where('tanggal',$row_htoemtd['tanggal'])
                     ->exec();
+                }
             }
         
         }elseif($state == 2){
@@ -117,6 +139,108 @@
                 ->where('id_htoxxth', $_POST['id_transaksi_h'] )
                 ->exec();
 
+            foreach ($rs_htoemtd as $row_htoemtd) {
+                $qs_htssctd = $db
+                    ->query('select', 'htssctd' )
+                    ->get(['id_htsxxmh'] )
+                    ->where('id_hemxxmh',$row_htoemtd['id_hemxxmh'])
+                    ->where('tanggal',$row_htoemtd['tanggal'])
+                    ->exec();
+                $rs_htssctd = $qs_htssctd->fetch();
+                
+                $qu_jadwal = $db
+                    ->query('update', 'htssctd')
+                    ->set('is_active', 0)
+                    ->where('id_hemxxmh',$row_htoemtd['id_hemxxmh'])
+                    ->where('tanggal',$row_htoemtd['tanggal'])
+                ->exec();
+
+                // BEGIN insert pengganti
+                $qr_htssctd = $db
+                ->raw()
+                ->bind(':id_hemxxmh', $row_htoemtd['id_hemxxmh'])
+                ->bind(':id_htsxxmh', $rs_htssctd['id_htsxxmh'])
+                ->bind(':tanggal', $row_htoemtd["tanggal"])
+                ->exec('
+                    INSERT INTO htssctd
+                    (
+                        id_hemxxmh,
+                        id_htsxxmh,
+                        tanggal,
+                        jam_awal,
+                        jam_akhir,
+                        jam_awal_istirahat,
+                        jam_akhir_istirahat,
+                        menit_toleransi_awal_in,
+                        menit_toleransi_akhir_in,
+                        menit_toleransi_awal_out,
+                        menit_toleransi_akhir_out,
+
+                        tanggaljam_awal_t1,
+                        tanggaljam_awal,
+                        tanggaljam_awal_t2,
+                        tanggaljam_akhir_t1,
+                        tanggaljam_akhir,
+                        tanggaljam_akhir_t2,
+                        tanggaljam_awal_istirahat,
+                        tanggaljam_akhir_istirahat
+                    )
+                    SELECT
+                        :id_hemxxmh,
+                        :id_htsxxmh,
+                        :tanggal,
+                        htsxxmh.jam_awal,
+                        htsxxmh.jam_akhir,
+                        htsxxmh.jam_awal_istirahat,
+                        htsxxmh.jam_akhir_istirahat,
+                        htsxxmh.menit_toleransi_awal_in,
+                        htsxxmh.menit_toleransi_akhir_in,
+                        htsxxmh.menit_toleransi_awal_out,
+                        htsxxmh.menit_toleransi_akhir_out,
+
+                        CONCAT(:tanggal, " ", TIME(DATE_SUB(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_awal_in MINUTE))) AS tanggaljam_awal_t1,
+                        CONCAT(:tanggal, " ", htsxxmh.jam_awal) AS tanggaljam_awal,
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN DATE_ADD(:tanggal, INTERVAL 1 DAY)
+                                ELSE :tanggal
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(htsxxmh.jam_awal, INTERVAL htsxxmh.menit_toleransi_akhir_in MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_awal_t2,
+                        CONCAT(:tanggal, " ", TIME(DATE_SUB(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_awal_out MINUTE))) AS tanggaljam_akhir_t1,
+                        CONCAT(:tanggal, " ", htsxxmh.jam_akhir) AS tanggaljam_akhir,
+                        
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE) >= "24:00:00" 
+                                    OR htsxxmh.kode like "malam%" AND htsxxmh.jam_akhir <= "12:00:00" THEN DATE_ADD(:tanggal, INTERVAL 1 DAY)
+                                ELSE :tanggal
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(htsxxmh.jam_akhir, INTERVAL htsxxmh.menit_toleransi_akhir_out MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_akhir_t2,
+                        CONCAT(:tanggal, " ", htsxxmh.jam_awal_istirahat) AS tanggaljam_awal_istirahat,
+                        CONCAT(:tanggal, " ", htsxxmh.jam_akhir_istirahat) AS tanggaljam_akhir_istirahat
+                    FROM htsxxmh
+                    WHERE 
+                        id = :id_htsxxmh
+                ');
+            }
         }
 
         $db->commit();
