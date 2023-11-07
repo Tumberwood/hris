fb0c6b224e6c2a1ee3899a8e6008ddac03ea972d
Ferry Ibnu <ferryibnu86@gmail.com>
2023-11-07 11:35:27 +0700

    f api pegawai

---
+++ usersc/api/pegawai/employee.php
@@ -0,0 +1,23 @@
+<?php
+require_once "method.php";
+$mhs = new Employee();
+$request_method=$_SERVER["REQUEST_METHOD"];
+switch ($request_method) {
+	case 'GET':
+			if(!empty($_GET["id"]))
+			{
+				$id=intval($_GET["id"]);
+				$mhs->get_emp($id);
+			}
+			else 
+			{
+				$mhs->get_all_emp();
+			}
+			break;
+	default:
+		// Invalid Request Method
+			header("HTTP/1.0 405 Method Not Allowed");
+			break;
+		break;
+}
+?>
