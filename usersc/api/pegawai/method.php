292d187d6cbc5bea724975c4dd1fe98d600561e8
Ferry Ibnu <ferryibnu86@gmail.com>
2023-11-08 06:21:22 +0700

    f method api

--- usersc/api/pegawai/method.php
+++ usersc/api/pegawai/method.php
@@ -27,21 +27,21 @@
 			$credentials = base64_encode("$username:$password");
 			$pass = 'Basic '.$credentials;
 		
-			if (!$token) {
-				http_response_code(401);
-				echo json_encode(array("message" => "Unauthorized"));
-				exit();
-			}
+			// if (!$token) {
+			// 	http_response_code(401);
+			// 	echo json_encode(array("message" => "Unauthorized"));
+			// 	exit();
+			// }
 		
 			try {
-				if ($token == $pass) {
-					$decoded = array('HS256');
-					return $decoded;
-				} else {
-					http_response_code(401);
-					echo json_encode(array("message" => "Invalid token"));
-					exit();
-				}
+				// if ($token == $pass) {
+				// 	$decoded = array('HS256');
+				// 	return $decoded;
+				// } else {
+				// 	http_response_code(401);
+				// 	echo json_encode(array("message" => "Invalid token"));
+				// 	exit();
+				// }
 				
 				echo $token . '<br>';
 				echo $credentials;
