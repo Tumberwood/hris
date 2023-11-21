9c62c7905bba633c82a6e3fec0cddfa4ea860656
Ferry Ibnu <ferryibnu86@gmail.com>
2023-11-21 12:19:31 +0700

    f maskur presensi OK

--- usersc/applications/models/htsprrd/fn_presensi_ok.php
+++ usersc/applications/models/htsprrd/fn_presensi_ok.php
@@ -358,7 +358,7 @@ use
             ->exec();
         }
 
-        if ($id_hemxxmh_select = 130) {
+        if ($id_hemxxmh_select == 130) {
             $sql_update = $editor->db()
                 ->query('update', 'htsprrd')
                 ->set('cek', 0)
