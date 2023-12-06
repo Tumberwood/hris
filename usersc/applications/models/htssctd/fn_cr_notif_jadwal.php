b40d2218ccd72b5bec2250e64ed4ec805d32b2d3
Ferry Ibnu <ferryibnu86@gmail.com>
2023-12-06 14:19:01 +0700

    f cronjob

--- usersc/applications/models/htssctd/fn_cr_notif_jadwal.php
+++ usersc/applications/models/htssctd/fn_cr_notif_jadwal.php
@@ -114,7 +114,7 @@
 	if($from != NULL && $currentPage == $filename) {
 		$qi_ = $db
 			->query('insert', 'crons_logs_si')
-			->set('name', "emailserver")
+			->set('name', "jadwal_belum_dibuat")
 			->set('created_by',1)
 			->set('created_on',$today)
 			->exec();
