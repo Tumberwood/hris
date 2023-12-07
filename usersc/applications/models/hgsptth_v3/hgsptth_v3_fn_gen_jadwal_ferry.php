f453e43c8836ab172d49c8e54334718d78e2d6cd
Ferry Ibnu <ferryibnu86@gmail.com>
2023-12-07 16:03:48 +0700

    f gen jadwal

--- usersc/applications/models/hgsptth_v3/hgsptth_v3_fn_gen_jadwal_ferry.php
+++ usersc/applications/models/hgsptth_v3/hgsptth_v3_fn_gen_jadwal_ferry.php
@@ -164,7 +164,7 @@
                     WHERE a.tanggal_awal = :dari_tanggal AND a.is_active = 1 AND (b.id_htsxxmh <> 1 AND b.shift <> 5)
                 
             ');
-    
+            // Sabtu
             $qi_jadwal = $db
             ->raw()
             ->bind(':id_hgsptth_v3', $id_hgsptth_v3)
@@ -272,11 +272,231 @@
                     LEFT JOIN htsxxmh AS d ON d.id = a.id_htsxxmh
                     WHERE
                         b.id = :id_hgsptth_v3
-                        AND CASE
-                            WHEN a.nama = "sabtu" THEN DAYOFWEEK(c.sabtu) = 7
-                            WHEN a.nama = "minggu" THEN DAYOFWEEK(c.sabtu) = 1
-                            ELSE DAYOFWEEK(c.sabtu) BETWEEN 2 AND 6
-                        END;
+                        AND a.nama = "sabtu" THEN DAYOFWEEK(c.sabtu) = 7;
+                    '
+            );
+            
+            // Minggu
+            $qi_jadwal = $db
+            ->raw()
+            ->bind(':id_hgsptth_v3', $id_hgsptth_v3)
+            ->bind(':tanggal_awal', $tanggal_awal)
+            ->bind(':tanggal_akhir', $tanggal_akhir)
+            ->exec(' INSERT INTO htssctd
+                    (
+                        tanggal,
+                        id_hemxxmh,
+                        id_htsxxmh,
+                        jam_awal,
+                        jam_akhir,
+                        jam_awal_istirahat,
+                        jam_akhir_istirahat,
+                        menit_toleransi_awal_in,
+                        menit_toleransi_akhir_in,
+                        menit_toleransi_awal_out,
+                        menit_toleransi_akhir_out,
+    
+                        tanggaljam_awal_t1,
+                        tanggaljam_awal,
+                        tanggaljam_awal_t2,
+                        tanggaljam_akhir_t1,
+                        tanggaljam_akhir,
+                        tanggaljam_akhir_t2,
+                        tanggaljam_awal_istirahat,
+                        tanggaljam_akhir_istirahat
+                    )
+                    WITH RECURSIVE date_range AS (
+                        SELECT DATE(:tanggal_awal) AS sabtu
+                        UNION ALL
+                        SELECT DATE_ADD(sabtu, INTERVAL 1 DAY)
+                        FROM date_range
+                        WHERE sabtu < :tanggal_akhir
+                    )
+                    SELECT DISTINCT
+                        c.sabtu,
+                        a.id_hemxxmh,
+                        a.id_htsxxmh,
+                        d.jam_awal,
+                        d.jam_akhir,
+                        d.jam_awal_istirahat,
+                        d.jam_akhir_istirahat,
+                        d.menit_toleransi_awal_in,
+                        d.menit_toleransi_awal_out,
+                        d.menit_toleransi_akhir_in,
+                        d.menit_toleransi_akhir_out,
+                        concat(c.sabtu, " ", TIME(DATE_SUB(d.jam_awal, INTERVAL d.menit_toleransi_awal_in MINUTE))) AS tanggaljam_awal_t1,
+                        concat(c.sabtu, " ", d.jam_awal) AS tanggaljam_awal,
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(d.jam_awal, INTERVAL d.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN DATE_ADD(c.sabtu, INTERVAL 1 DAY)
+                                ELSE c.sabtu
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(d.jam_awal, INTERVAL d.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(d.jam_awal, INTERVAL d.menit_toleransi_akhir_in MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(d.jam_awal, INTERVAL d.menit_toleransi_akhir_in MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_awal_t2,
+    
+                        CASE
+                            WHEN d.kode like "malam%" AND d.jam_akhir <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(c.sabtu, INTERVAL 1 DAY), " ", TIME(DATE_SUB(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE)))
+                            ELSE CONCAT(c.sabtu, " ", TIME(DATE_SUB(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE)))
+                        END AS tanggaljam_akhir_t1,
+                        CASE
+                            WHEN d.kode like "malam%" AND d.jam_akhir <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(c.sabtu, INTERVAL 1 DAY), " ", d.jam_akhir)
+                            ELSE CONCAT(c.sabtu, " ", d.jam_akhir)
+                        END AS tanggaljam_akhir,
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE) >= "24:00:00" 
+                                    OR d.kode like "malam%" AND d.jam_akhir <= "12:00:00" THEN DATE_ADD(c.sabtu, INTERVAL 1 DAY)
+                                ELSE c.sabtu
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_akhir_t2,
+                        CASE
+                            WHEN d.kode like "malam%" AND d.jam_awal_istirahat <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(c.sabtu, INTERVAL 1 DAY), " ", d.jam_awal_istirahat)
+                            ELSE CONCAT(c.sabtu, " ", d.jam_awal_istirahat)
+                        END AS tanggaljam_awal_istirahat,
+                        CASE
+                            WHEN d.kode like "malam%" AND d.jam_akhir_istirahat <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(c.sabtu, INTERVAL 1 DAY), " ", d.jam_akhir_istirahat)
+                            ELSE CONCAT(c.sabtu, " ", d.jam_akhir_istirahat)
+                        END AS tanggaljam_akhir_istirahat
+                    FROM hgsemtd_v3 AS a
+                    LEFT JOIN hgsptth_v3 AS b ON b.id = a.id_hgsptth_v3
+                    LEFT JOIN date_range AS c ON c.sabtu BETWEEN b.tanggal_awal AND b.tanggal_akhir
+                    LEFT JOIN htsxxmh AS d ON d.id = a.id_htsxxmh
+                    WHERE
+                        b.id = :id_hgsptth_v3
+                        AND a.nama = "minggu" THEN DAYOFWEEK(c.sabtu) = 1;
+                    '
+            );
+            
+            // Senjum
+            $qi_jadwal = $db
+            ->raw()
+            ->bind(':id_hgsptth_v3', $id_hgsptth_v3)
+            ->bind(':tanggal_awal', $tanggal_awal)
+            ->bind(':tanggal_akhir', $tanggal_akhir)
+            ->exec(' INSERT INTO htssctd
+                    (
+                        tanggal,
+                        id_hemxxmh,
+                        id_htsxxmh,
+                        jam_awal,
+                        jam_akhir,
+                        jam_awal_istirahat,
+                        jam_akhir_istirahat,
+                        menit_toleransi_awal_in,
+                        menit_toleransi_akhir_in,
+                        menit_toleransi_awal_out,
+                        menit_toleransi_akhir_out,
+    
+                        tanggaljam_awal_t1,
+                        tanggaljam_awal,
+                        tanggaljam_awal_t2,
+                        tanggaljam_akhir_t1,
+                        tanggaljam_akhir,
+                        tanggaljam_akhir_t2,
+                        tanggaljam_awal_istirahat,
+                        tanggaljam_akhir_istirahat
+                    )
+                    WITH RECURSIVE date_range AS (
+                        SELECT DATE(:tanggal_awal) AS sabtu
+                        UNION ALL
+                        SELECT DATE_ADD(sabtu, INTERVAL 1 DAY)
+                        FROM date_range
+                        WHERE sabtu < :tanggal_akhir
+                    )
+                    SELECT DISTINCT
+                        c.sabtu,
+                        a.id_hemxxmh,
+                        a.id_htsxxmh,
+                        d.jam_awal,
+                        d.jam_akhir,
+                        d.jam_awal_istirahat,
+                        d.jam_akhir_istirahat,
+                        d.menit_toleransi_awal_in,
+                        d.menit_toleransi_awal_out,
+                        d.menit_toleransi_akhir_in,
+                        d.menit_toleransi_akhir_out,
+                        concat(c.sabtu, " ", TIME(DATE_SUB(d.jam_awal, INTERVAL d.menit_toleransi_awal_in MINUTE))) AS tanggaljam_awal_t1,
+                        concat(c.sabtu, " ", d.jam_awal) AS tanggaljam_awal,
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(d.jam_awal, INTERVAL d.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN DATE_ADD(c.sabtu, INTERVAL 1 DAY)
+                                ELSE c.sabtu
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(d.jam_awal, INTERVAL d.menit_toleransi_akhir_in MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(d.jam_awal, INTERVAL d.menit_toleransi_akhir_in MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(d.jam_awal, INTERVAL d.menit_toleransi_akhir_in MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_awal_t2,
+    
+                        CASE
+                            WHEN d.kode like "malam%" AND d.jam_akhir <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(c.sabtu, INTERVAL 1 DAY), " ", TIME(DATE_SUB(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE)))
+                            ELSE CONCAT(c.sabtu, " ", TIME(DATE_SUB(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE)))
+                        END AS tanggaljam_akhir_t1,
+                        CASE
+                            WHEN d.kode like "malam%" AND d.jam_akhir <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(c.sabtu, INTERVAL 1 DAY), " ", d.jam_akhir)
+                            ELSE CONCAT(c.sabtu, " ", d.jam_akhir)
+                        END AS tanggaljam_akhir,
+                        CONCAT(
+                            CASE
+                                WHEN DATE_ADD(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE) >= "24:00:00" 
+                                    OR d.kode like "malam%" AND d.jam_akhir <= "12:00:00" THEN DATE_ADD(c.sabtu, INTERVAL 1 DAY)
+                                ELSE c.sabtu
+                            END,
+                            " ",
+                            TIME(
+                                CASE
+                                    WHEN DATE_ADD(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE) >= "24:00:00" THEN
+                                        TIMEDIFF(DATE_ADD(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE), "24:00:00")
+                                    ELSE
+                                        DATE_ADD(d.jam_akhir, INTERVAL d.menit_toleransi_akhir_out MINUTE)
+                                END
+                            )
+                        ) AS tanggaljam_akhir_t2,
+                        CASE
+                            WHEN d.kode like "malam%" AND d.jam_awal_istirahat <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(c.sabtu, INTERVAL 1 DAY), " ", d.jam_awal_istirahat)
+                            ELSE CONCAT(c.sabtu, " ", d.jam_awal_istirahat)
+                        END AS tanggaljam_awal_istirahat,
+                        CASE
+                            WHEN d.kode like "malam%" AND d.jam_akhir_istirahat <= "12:00:00"
+                            THEN CONCAT(DATE_ADD(c.sabtu, INTERVAL 1 DAY), " ", d.jam_akhir_istirahat)
+                            ELSE CONCAT(c.sabtu, " ", d.jam_akhir_istirahat)
+                        END AS tanggaljam_akhir_istirahat
+                    FROM hgsemtd_v3 AS a
+                    LEFT JOIN hgsptth_v3 AS b ON b.id = a.id_hgsptth_v3
+                    LEFT JOIN date_range AS c ON c.sabtu BETWEEN b.tanggal_awal AND b.tanggal_akhir
+                    LEFT JOIN htsxxmh AS d ON d.id = a.id_htsxxmh
+                    WHERE
+                        b.id = :id_hgsptth_v3
+                        AND DAYOFWEEK(c.sabtu) BETWEEN 2 AND 6;
                     '
             );
             
