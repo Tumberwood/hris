dbb9d864b752ea00c6a5d5108df47ab299430080
unknown <ferryibnu86@gmail.com>
2025-04-16 15:31:03 +0700

    f fixed colums

--- usersc/applications/views/htsprrd_overtime_jam/htsprrd_overtime_jam.php
+++ usersc/applications/views/htsprrd_overtime_jam/htsprrd_overtime_jam.php
@@ -303,6 +303,9 @@
 						targets: [4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21, 22, 23, 24,25]
 					}
 				],
+				fixedColumns: {
+                    leftColumns: 2 // Freeze column 0 and 1
+                },
 				ajax: {
 					url: "../../models/htsprrd_overtime/htsprrd.php",
 					type: 'POST',
