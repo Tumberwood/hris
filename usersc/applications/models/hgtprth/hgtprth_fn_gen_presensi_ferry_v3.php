ba4c0f66e37f0235775da444cccaa4609cdede24
unknown <ferryibnu86@gmail.com>
2025-04-17 09:34:18 +0700

    f gen presensi

--- usersc/applications/models/hgtprth/hgtprth_fn_gen_presensi_ferry_v3.php
+++ usersc/applications/models/hgtprth/hgtprth_fn_gen_presensi_ferry_v3.php
@@ -706,21 +706,44 @@
 
                                         -- ceklok ISTIRAHAT
                                         LEFT JOIN (
-                                            SELECT DISTINCT
-                                                a.id_hemxxmh,
-                                                a.id AS id_jadwal,
-                                                concat(c.tanggal," ",c.jam) AS ceklok_istirahat,
-                                                TIMESTAMPDIFF(MINUTE,MIN(CONCAT(c.tanggal," ",c.jam)),	MAX(CONCAT(c.tanggal," ",c.jam))) as durasi_break_menit
-                                            FROM htssctd AS a
-                                            INNER JOIN hemxxmh AS b ON b.id = a.id_hemxxmh
-                                            INNER JOIN htsprtd AS c ON c.kode = b.kode_finger
-                                            WHERE a.tanggal = :tanggal AND a.is_active = 1 AND b.is_active = 1
-                                                AND c.nama IN ("os", "out", "staff", "PMI", "PMI-Gedung-3", "OS-Gedung-3", "istirahat", "istirahat manual")
-                                                AND TIMESTAMP(c.tanggal, c.jam) BETWEEN a.tanggaljam_awal_istirahat AND DATE_ADD(a.tanggaljam_akhir_istirahat, INTERVAL 2 HOUR)
-                                                AND a.id_hemxxmh IN '.$id_hemxxmh.'
-                                            GROUP BY a.id
-                                            ORDER BY ceklok_istirahat
+                                            SELECT *
+                                            FROM (
+                                                SELECT DISTINCT
+                                                    a.id_hemxxmh,
+                                                    a.id AS id_jadwal,
+                                                    CONCAT(c.tanggal," ",c.jam) AS ceklok_istirahat,
+                                                    TIMESTAMPDIFF(MINUTE, MIN(CONCAT(c.tanggal," ",c.jam)), MAX(CONCAT(c.tanggal," ",c.jam))) AS durasi_break_menit
+                                                FROM htssctd AS a
+                                                INNER JOIN hemxxmh AS b ON b.id = a.id_hemxxmh
+                                                INNER JOIN htsprtd AS c ON c.kode = b.kode_finger
+                                                WHERE a.tanggal = :tanggal AND a.is_active = 1 AND b.is_active = 1
+                                                    AND c.nama IN ("os", "out", "staff", "PMI", "PMI-Gedung-3", "OS-Gedung-3", "istirahat", "istirahat manual")
+                                                    AND CONCAT(c.tanggal, " ", c.jam) BETWEEN a.tanggaljam_awal_istirahat AND DATE_ADD(a.tanggaljam_akhir_istirahat, INTERVAL 2 HOUR)
+                                                    AND a.id_hemxxmh IN '.$id_hemxxmh.'
+                                                GROUP BY a.id
 
+                                                UNION ALL
+
+                                                SELECT DISTINCT
+                                                    a.id_hemxxmh,
+                                                    a.id AS id_jadwal,
+                                                    CONCAT(d.tanggal, " ", d.jam_awal) AS ceklok_istirahat,
+                                                    TIMESTAMPDIFF(MINUTE, MIN(CONCAT(c.tanggal," ",c.jam)), MAX(CONCAT(c.tanggal," ",c.jam))) AS durasi_break_menit
+                                                FROM htssctd AS a
+                                                INNER JOIN hemxxmh AS b ON b.id = a.id_hemxxmh
+                                                INNER JOIN htsprtd AS c ON c.kode = b.kode_finger
+                                                INNER JOIN htoxxrd AS d ON d.id_hemxxmh = a.id_hemxxmh AND d.tanggal = a.tanggal
+                                                WHERE a.tanggal = :tanggal AND a.is_active = 1 AND b.is_active = 1
+                                                    AND c.nama IN ("istirahat", "istirahat manual")
+                                                    AND CONCAT(c.tanggal, " ", c.jam) BETWEEN CONCAT(d.tanggal, " ", d.jam_awal) 
+                                                    AND CONCAT(d.tanggal, " ", d.jam_akhir) 
+                                                    AND a.id_hemxxmh IN '.$id_hemxxmh.'
+                                                    AND a.id_htsxxmh = 1
+                                                GROUP BY a.id
+                                            ) AS t
+                                            ORDER BY ceklok_istirahat
+                                            LIMIT 1
+                                                
                                         ) AS cek_istirahat ON cek_istirahat.id_hemxxmh = hto.id_hemxxmh
 
                                         -- menit_toleransi_ti settingan
