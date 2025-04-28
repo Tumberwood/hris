ebb52387ec92f951221170567180d926c1e614ae
unknown <ferryibnu86@gmail.com>
2025-04-28 14:52:10 +0700

    f gen presensi

--- usersc/applications/models/hgtprth_single/gen_presensi_single_backdoor.php
+++ usersc/applications/models/hgtprth_single/gen_presensi_single_backdoor.php
@@ -21,9 +21,9 @@
     $id_hemxxmh = '(1710,1709,1708,1707,1706)';
 
     // Mulai dari 10 Mei 2024
-    $start = Carbon::create(2024, 11, 1);
+    $start = Carbon::create(2024, 12, 1);
     // Sampai akhir Mei 2024
-    $end = Carbon::create(2024, 11, 30); 
+    $end = Carbon::create(2024, 12, 31); 
     
     // Buat periode
     $periode = CarbonPeriod::create($start, $end);
