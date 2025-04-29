fc351332470a3e3847aadb62d2638a428e9b2395
unknown <ferryibnu86@gmail.com>
2025-04-29 14:03:44 +0700

    f wpkxxth

--- usersc/applications/views/hgtprth_single/hgtprth_single.php
+++ usersc/applications/views/hgtprth_single/hgtprth_single.php
@@ -166,7 +166,7 @@
 							allowClear: true,
 							multiple: false,
 							ajax: {
-								url: "../../models/hemxxmh/hemxxmh_fn_opt.php",
+								url: "../../models/hemxxmh/hemxxmh_fn_opt_all.php",
 								dataType: 'json',
 								data: function (params) {
 									var query = {
