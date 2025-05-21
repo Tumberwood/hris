eb3e1b8be63df50b48fffb7e34b37c63930d8055
unknown <ferryibnu86@gmail.com>
2025-05-21 15:45:49 +0700

    f htsprrd 2

--- usersc/applications/views/htsprrd_2/htsprrd_2.php
+++ usersc/applications/views/htsprrd_2/htsprrd_2.php
@@ -376,13 +376,13 @@
 						searchPanes:{
 							show: true
 						},
-						targets: [1,3,5,6,12,13,14,15]
+						targets: [1,3,5,6,7,12,13,14,15]
 					},
 					{
 						searchPanes:{
 							show: false
 						},
-						targets: [0,2,4,7,8,9,10,11,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]
+						targets: "_all"
 					}
 				],
 				ajax: {
