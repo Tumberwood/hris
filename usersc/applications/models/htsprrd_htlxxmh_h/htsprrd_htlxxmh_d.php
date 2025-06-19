3d066f8e91706d21b31a48b5af2e4a2ac805b3fa
unknown <ferryibnu86@gmail.com>
2025-06-19 12:32:12 +0700

    f ht

--- usersc/applications/models/htsprrd_htlxxmh_h/htsprrd_htlxxmh_d.php
+++ usersc/applications/models/htsprrd_htlxxmh_h/htsprrd_htlxxmh_d.php
@@ -105,7 +105,20 @@
                             CASE 
 								WHEN a.status_presensi_out = "MK" THEN 1
                             	WHEN a.status_presensi_in = "MK" THEN 1
-								WHEN a.pot_hk > 0 THEN 1
+                            	ELSE 0 
+							END
+						) 
+						+
+						SUM(
+                            CASE 
+								WHEN 
+									a.status_presensi_out <> "MK" AND
+                            	 	a.status_presensi_in <> "MK" AND
+									a.status_presensi_in <> "TL" AND st_clock_in <> "LATE" AND
+									a.st_clock_in <> "LATE" AND
+									a.status_presensi_out <> "PA" AND
+									a.st_clock_out <> "EARLY" AND
+									a.pot_hk > 0 THEN 1
                             	ELSE 0 
 							END
 						) 
