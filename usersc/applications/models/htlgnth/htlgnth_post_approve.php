e47519cfd11a5fe2f08dbc90832a506b61a95ec1
unknown <ferryibnu86@gmail.com>
2025-06-19 09:41:30 +0700

    f cuti bersama approve

--- usersc/applications/models/htlgnth/htlgnth_post_approve.php
+++ usersc/applications/models/htlgnth/htlgnth_post_approve.php
@@ -51,7 +51,7 @@
                 AND a.tanggal = :tanggal
                 AND NOT (
                     id_hetxxmh IN (99, 48) -- kecuali jabatan Petugas Satpam & Koordinator Satpam
-                    OR id_heyxxmd = 1  -- kecuali KBM
+                    -- OR id_heyxxmd = 1  -- kecuali KBM
                     OR (id_heyxxmd = 2 AND id_hesxxmh = 2)  -- staff dengan status kontrak
                     OR (id_heyxxmd = 3 AND id_hesxxmh = 2) -- karyawan dengan status kontrak
                     OR id_htsxxmh = 1 -- off
