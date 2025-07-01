20742db4b539c5e01d881fc60b6318669cd19d68
unknown <ferryibnu86@gmail.com>
2025-07-01 14:59:49 +0700

    f

--- usersc/applications/models/htsprrd_htlxxmh_h/htsprrd_htlxxmh_d.php
+++ usersc/applications/models/htsprrd_htlxxmh_h/htsprrd_htlxxmh_d.php
@@ -121,9 +121,11 @@
 									a.status_presensi_out <> "PA" AND
 									a.st_clock_out <> "EARLY" AND
 									a.pot_hk > 0 
-									-- AND
-									-- (a.htlxxrh_kode NOT LIKE "PA%" AND a.st_clock_out <> "OK" AND a.pot_hk > 0) AND
-									-- (a.htlxxrh_kode NOT LIKE "TL%" AND a.st_clock_in <> "OK" AND a.pot_hk > 0)
+									THEN 1
+								WHEN 
+									(a.htlxxrh_kode NOT LIKE "PA%" AND a.st_clock_out <> "OK") AND
+									(a.htlxxrh_kode NOT LIKE "TL%" AND a.st_clock_in <> "OK") AND
+									a.pot_hk > 0
 									THEN 1
                             	ELSE 0 
 							END
