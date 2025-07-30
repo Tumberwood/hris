c0ffd444bc587faccd183e0d9a92f675816b2ddf
unknown <ferryibnu86@gmail.com>
2025-07-30 14:55:24 +0700

    f payroll ferry

--- usersc/applications/models/hpyxxth/hpyxxth_fn_gen_payroll_ferry_2025.php
+++ usersc/applications/models/hpyxxth/hpyxxth_fn_gen_payroll_ferry_2025.php
@@ -687,7 +687,7 @@
                                         id_hemxxmh,
                                         COUNT(id) AS report_pot_premi
                                     FROM htsprrd
-                                    WHERE tanggal BETWEEN :tanggal_awal AND (:tanggal_akhir)
+                                    WHERE tanggal BETWEEN DATE_FORMAT(:tanggal_awal, "%Y-%m-01") AND LAST_DAY(:tanggal_awal)
                                         AND is_pot_premi = 1
                                     GROUP BY id_hemxxmh
                                 ) c_report_pot_premi
