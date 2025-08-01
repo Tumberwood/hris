5109d2e8eb2f545ce8ed860b9b57d7391cb53430
unknown <ferryibnu86@gmail.com>
2025-08-01 09:14:16 +0700

    f komp gaji

---
+++ usersc/applications/models/report_komp_gaji/report_komp_gaji.php
@@ -0,0 +1,95 @@
+<?php
+	// tes webhook
+	include( "../../../../users/init.php" );
+	include( "../../../../usersc/lib/DataTables.php" );
+
+	require '../../../../usersc/vendor/autoload.php';
+	use Carbon\Carbon;
+	
+	use
+		DataTables\Editor,
+		DataTables\Editor\Query,
+		DataTables\Editor\Result;
+	
+	
+	$start_date = $_POST['start_date'];
+
+	$qs_htsprrd = $db
+		->raw()
+		->bind(':start_date', $start_date)
+		->exec('SELECT
+					a.id,
+					a.kode nik,
+					a.nama,
+					d.nama AS dep,
+					e.nama AS jab,
+					f.nama AS area,
+					g.nama AS level,
+					h.nama AS status,
+					i.nama AS tipe,
+					c.tanggal_masuk,
+					c.tanggal_keluar,
+					if(c.id_hesxxmh = 3, gp_pelatihan, nominal_gp) komp_gaji
+				FROM hemxxmh a
+				INNER JOIN hemjbmh c ON c.id_hemxxmh = a.id
+				INNER JOIN hodxxmh d ON d.id = c.id_hodxxmh
+				INNER JOIN hetxxmh e ON e.id = c.id_hetxxmh
+				LEFT JOIN holxxmd_2 f ON f.id = c.id_holxxmd_2
+				LEFT JOIN hevxxmh g ON g.id = c.id_hevxxmh
+				LEFT JOIN hesxxmh h ON h.id = c.id_hesxxmh
+				LEFT JOIN heyxxmd i ON i.id = c.id_heyxxmd
+				
+				-- gaji pokok
+				LEFT JOIN (
+					SELECT
+						id_hemxxmh,
+						tanggal_efektif,
+						nominal AS nominal_gp
+					FROM (
+						SELECT
+							id,
+							id_hemxxmh,
+							tanggal_efektif,
+							nominal,
+							ROW_NUMBER() OVER (PARTITION BY id_hemxxmh ORDER BY tanggal_efektif DESC) AS row_num
+						FROM htpr_hemxxmh
+						WHERE
+							htpr_hemxxmh.id_hpcxxmh = 1
+							AND tanggal_efektif < :start_date
+					) AS subquery
+					WHERE row_num = 1
+				) tbl_htpr_hemxxmh ON tbl_htpr_hemxxmh.id_hemxxmh = c.id_hemxxmh
+				
+				
+				-- gaji untuk pelatihan
+				LEFT JOIN (
+						SELECT
+							id_hesxxmh,
+							tanggal_efektif,
+							IFNULL(nominal, 0) AS gp_pelatihan
+						FROM (
+							SELECT
+								id,
+								id_hesxxmh,
+								tanggal_efektif,
+								nominal,
+								ROW_NUMBER() OVER (PARTITION BY id_hesxxmh ORDER BY tanggal_efektif DESC) AS row_num
+							FROM htpr_hesxxmh
+						WHERE
+							htpr_hesxxmh.id_hpcxxmh = 1
+							AND tanggal_efektif < :start_date
+					) AS subquery
+					WHERE row_num = 1
+				) lembur_mati ON lembur_mati.id_hesxxmh = c.id_hesxxmh
+				WHERE a.is_active = 1
+				HAVING komp_gaji IS NOT null
+				'
+				);
+	$rs_htsprrd = $qs_htsprrd->fetchAll();
+
+	$results = array();
+
+	$results['data']['htsprrd'] = !empty($rs_htsprrd) ? $rs_htsprrd : [];
+
+	echo json_encode($results);
+?>
---
+++ usersc/applications/views/report_komp_gaji/report_komp_gaji.php
@@ -0,0 +1,310 @@
+<?php
+    require_once '../../../../users/init.php';
+    require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
+    if (!securePage($_SERVER['PHP_SELF'])) {
+        die();
+    }
+?>
+
+<?php
+	$nama_tabel    = 'report_komp_gaji';
+	$nama_tabels_d = [];
+	
+	if (isset($_GET['id_hemxxmh'])){
+		$id_hemxxmh		= $_GET['id_hemxxmh'];
+	} else {
+		$id_hemxxmh		= 0;
+	}
+	if (isset($_GET['start_date'])){
+		$awal		= ($_GET['start_date']);
+	} else {
+		$awal = null;
+	}
+?>
+
+<!-- begin content here -->
+
+<div class="row">
+    <div class="col">
+        <div class="ibox collapsed" id="iboxfilter">
+            <div class="ibox-title">
+                <h5 class="text-navy">Filter</h5>&nbsp
+                <button class="btn btn-primary btn-xs collapse-link"><i class="fa fa-chevron-up"></i></button>
+            </div>
+            <div class="ibox-content">
+                <form class="form-horizontal" id="frmreport_komp_gaji">
+                    <div class="form-group row">
+                        <label class="col-lg-2 col-form-label">Periode</label>
+                        <div class="col-lg-5">
+                            <div class="input-group input-daterange" id="periode">
+                                <input type="text" id="start_date" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">												
+                        <label class="col-sm-2 col-form-label">Employee</label>
+                        <div class="col-sm-4">
+                            <select class="form-control" id="select_hemxxmh" name="select_hemxxmh"></select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-lg-4">
+                            <button class="btn btn-primary" type="submit" id="go">Submit</button>
+                        </div>
+                    </div>
+                </form>
+                <div id="searchPanes1"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<div class="ibox ">
+			<div class="ibox-content">
+				<div class="table-responsive">
+                    <table id="tblreport_komp_gaji" class="table table-striped table-bordered table-hover nowrap" width="100%">
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>NIK</th>
+								<th>Nama</th>
+								<th>Department</th>
+								<th>Jabatan</th>
+								<th>Level</th>
+								<th>Status</th>
+								<th>Type</th>
+								<th>Komp. Gaji</th>
+							</tr>
+						</thead>
+                    </table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- BEGIN JS -->
+<?php require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->template . '/template_js_load.php'; ?>
+<?php require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->template . '/template_js_setup.php'; ?>
+<?php require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->template . '/template_js_datatables_load.php'; ?>
+<script src="<?=$us_url_root?>usersc/helpers/hakaksescrud_hd_fn.js"></script>
+
+<!-- BEGIN datatables here -->
+<script type="text/javascript">
+		// ------------- default variable, do not erase
+		var tblreport_komp_gaji, show_inactive_status_report_komp_gaji = 0;
+		var id_hemxxmh = 0;
+		var id_hemxxmh_old = 0;
+		var id_hem_get = <?php echo $id_hemxxmh ?>;
+		var tanggal_get = "<?php echo $awal ?>";
+		
+		// ------------- end of default variable
+
+		// BEGIN datepicker init
+		$('#periode').datepicker({
+			setDate: new Date(),
+			autoclose: true,
+			todayHighlight: true,
+			clearBtn: true,
+			format: "dd M yyyy",
+			minViewMode: 'month' 
+		});
+		
+		$('#start_date').datepicker('setDate', tanggal_hariini_dmy);
+        // END datepicker init
+
+		//Select2 init
+        $("#select_hemxxmh").select2({
+			placeholder: 'Ketik atau TekanTanda Panah Kanan',
+			allowClear: true,
+			ajax: {
+				url: "../../models/hemxxmh/hemxxmh_fn_opt.php",
+				dataType: 'json',
+				data: function (params) {
+					var query = {
+						id_hemxxmh_old: id_hemxxmh_old,
+						search: params.term || '',
+						page: params.page || 1
+					}
+						return query;
+				},
+				processResults: function (data, params) {
+					if (id_hem_get > 0) {
+						var options = data.results.map(function (result) {
+							return {
+								id: result.id,
+								text: result.text
+							};
+						});
+
+						//add by ferry agar auto select 07 sep 23
+						if (params.page && params.page === 1) {
+							$('#select_hemxxmh').empty().select2({ data: options });
+						} else {
+							$('#select_hemxxmh').append(new Option(options[0].text, options[0].id, false, false)).trigger('change');
+						}
+
+						return {
+							results: options,
+							pagination: {
+								more: true
+							}
+						};
+					} else {
+						return {
+							results: data.results,
+							pagination: {
+								more: true
+							}
+						};
+					}
+				},
+				cache: true,
+				minimumInputLength: 1,
+				maximum: 10,
+				delay: 500,
+				maximumSelectionLength: 5,
+				minimumResultsForSearch: -1,
+			}
+			
+		});
+        // END select2 init
+		
+		$(document).ready(function() {
+			start_date = moment($('#start_date').val()).format('YYYY-MM-DD');
+			
+			id_hemxxmh_old = id_hem_get;
+			
+			$('#select_hemxxmh').select2('open');
+
+			setTimeout(function() {
+				$('#select_hemxxmh').select2('close');
+			}, 5);
+
+			//start datatables
+			tblreport_komp_gaji = $('#tblreport_komp_gaji').DataTable( {
+				searchPanes:{
+					layout: 'columns-3',
+				},
+				dom: 
+					"<P>"+
+					"<lf>"+
+					"<B>"+
+					"<rt>"+
+					"<'row'<'col-sm-4'i><'col-sm-8'p>>",
+				columnDefs:[
+					{
+						searchPanes:{
+							show: true,
+						},
+						targets: [1,3,4,5,6]
+					},
+					{
+						searchPanes:{
+							show: false,
+						},
+						targets: '_all'
+					}
+				],
+				ajax: {
+					url: "../../models/report_komp_gaji/report_komp_gaji.php",
+					type: 'POST',
+					data: function (d){
+						d.start_date = start_date;
+						d.id_hemxxmh = id_hemxxmh;
+					},
+					dataSrc: 'data.htsprrd'
+				},
+				order: [[ 1, "asc" ]],
+				responsive: false,
+				columns: [
+					{ data: "id", visible: false },
+					{ data: "nik" },
+					{ data: "nama" },
+					{ data: "dep" },
+					{ data: "jab" },
+					{ data: "level" },
+					{ data: "status" },
+					{ data: "tipe" },
+					{ 
+						data: "komp_gaji",
+						render: $.fn.dataTable.render.number( ',', '.', 2,'','' ),
+						class: "text-right"
+					},
+				],
+				buttons: [
+					// BEGIN breaking generate button
+					<?php
+						$id_table    = 'id_prcittd';
+						$table       = 'tblprcittd';
+						$edt         = 'edtprcittd';
+						$show_status = '_prcittd';
+						$table_name  = $nama_tabel;
+
+						$arr_buttons_tools = ['copy','excel','colvis'];
+						$arr_buttons_action = [];
+						$arr_buttons_approve = [];
+						include $abs_us_root.$us_url_root. 'usersc/helpers/button_fn_generate.php'; 
+					?>
+					// END breaking generate button
+				],
+				rowCallback: function( row, data, index ) {
+				},
+				initComplete: function() {
+					this.api().searchPanes.rebuildPane();
+				},
+			} );
+
+			tblreport_komp_gaji.searchPanes.container().appendTo( '#searchPanes1' );
+
+			$("#frmreport_komp_gaji").submit(function(e) {
+				e.preventDefault();
+			}).validate({
+				rules: {
+					
+				},
+				submitHandler: function(frmreport_komp_gaji) {
+					start_date 		= moment($('#start_date').val()).format('YYYY-MM-DD');
+					if ($('#select_hemxxmh').val() > 0) {
+						id_hemxxmh = $('#select_hemxxmh').val();
+					} else {
+						if (id_hem_get != 0) {
+							id_hemxxmh = id_hem_get;
+						} else {
+							id_hemxxmh = $('#select_hemxxmh').val();
+						}
+					}
+
+					notifyprogress = $.notify({
+						message: 'Processing ...</br> Jangan tutup halaman sampai notifikasi ini hilang!'
+					},{
+						z_index: 9999,
+						allow_dismiss: false,
+						type: 'info',
+						delay: 0
+					});
+
+					tblreport_komp_gaji.rows().deselect();
+					tblreport_komp_gaji.ajax.reload(function ( json ) {
+						notifyprogress.close();
+					}, false);
+					return false; 
+				}
+			});
+			
+			if (id_hem_get > 0) {
+				$("#frmreport_komp_gaji").submit();
+			}
+			
+		} );// end of document.ready
+	
+	</script>
+
+<!-- END datatables here -->
+
+<!-- end content here -->
+
+<!-- do not erase -->
+<?php require_once $abs_us_root.$us_url_root.'users/includes/html_footer.php'; ?>
