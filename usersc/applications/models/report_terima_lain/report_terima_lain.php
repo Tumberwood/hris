71bef3c972cef146ea15c5d0fd52ee8f2b1a8139
unknown <ferryibnu86@gmail.com>
2025-09-26 10:40:11 +0700

    f terima lain

--- usersc/applications/models/report_terima_lain/report_terima_lain.php
+++ usersc/applications/models/report_terima_lain/report_terima_lain.php
@@ -39,8 +39,8 @@
                 SUM( ABS(IFNULL(a.abnormal,0)) ) abnormal,
                 0 AS selisih,
                 (-SUM(a.pot_hk) + SUM(ABS(IFNULL(a.abnormal, 0)))) AS total,
-                ( ( IFNULL(nominal_gp,0) + IFNULL(nominal_t_jab,0)) / 173) AS pengali,
-                (-SUM(a.pot_hk) + SUM(ABS(IFNULL(a.abnormal, 0)))) * ( ( IFNULL(nominal_gp,0) + IFNULL(nominal_t_jab,0)) / 173) AS terima_lain
+                ( ( IFNULL(nominal_gp,0) + + IF(c.id_heyxxmd = 1 AND c.id_hesxxmh = 4, COALESCE(nominal_jabatan, 0), COALESCE(nominal_t_jab, 0) )) / 173) AS pengali,
+                (-SUM(a.pot_hk) + SUM(ABS(IFNULL(a.abnormal, 0)))) * ( ( IFNULL(nominal_gp,0) + + IF(c.id_heyxxmd = 1 AND c.id_hesxxmh = 4, COALESCE(nominal_jabatan, 0), COALESCE(nominal_t_jab, 0) )) / 173) AS terima_lain
 
             FROM htsprrd a
             INNER JOIN hemxxmh b ON b.id = a.id_hemxxmh
@@ -89,6 +89,28 @@
                 ) AS subquery
                 WHERE row_num = 1
             ) t_jabatan ON t_jabatan.id_hevxxmh = c.id_hevxxmh
+
+            -- nominal tunjangan jabatan di menu per karyawan
+            LEFT JOIN (
+                SELECT
+                    id_hemxxmh,
+                    tanggal_efektif,
+                    IFNULL(nominal, 0) AS nominal_jabatan
+                FROM (
+                    SELECT
+                        id,
+                        id_hemxxmh,
+                        tanggal_efektif,
+                        nominal,
+                        ROW_NUMBER() OVER (PARTITION BY id_hemxxmh ORDER BY tanggal_efektif DESC) AS row_num
+                    FROM htpr_hemxxmh
+                    WHERE
+                        htpr_hemxxmh.id_hpcxxmh = 32
+                        AND tanggal_efektif <= :start_date
+                        AND is_active = 1
+                ) AS subquery
+                WHERE row_num = 1
+            ) tbl_jabatan ON tbl_jabatan.id_hemxxmh = b.id
 
             WHERE a.tanggal BETWEEN :start_date AND :end_date AND c.id_heyxxmd = 1
             GROUP BY a.id_hemxxmh
