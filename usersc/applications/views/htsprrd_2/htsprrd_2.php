fdadb843110d968e7e751d5f9246056787e88758
Ferry Ibnu <ferryibnu86@gmail.com>
2025-11-05 11:09:04 +0700

    f htsprrd 2

--- usersc/applications/views/htsprrd_2/htsprrd_2.php
+++ usersc/applications/views/htsprrd_2/htsprrd_2.php
@@ -1,4 +1,4 @@
-<?php
+Br<?php
     require_once '../../../../users/init.php';
     require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
     if (!securePage($_SERVER['PHP_SELF'])) {
@@ -127,8 +127,8 @@
 												<th rowspan=2>Jadwal</th>
 												<th rowspan=2>Clock In</th>
 												<th rowspan=2>Clock Out</th>
-												<th rowspan=2>Break In</th>
-												<th rowspan=2>Break Out</th>
+												<th rowspan=2>Istirahat In</th>
+												<th rowspan=2>Istirahat Out</th>
 												<th rowspan=2>Cek CI</th>
 												<th rowspan=2>Cek CO</th>
 												
