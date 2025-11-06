03320868b56bb3e8be75c51e44b64aeced15adc1
Ferry Ibnu <ferryibnu86@gmail.com>
2025-11-06 10:15:19 +0700

    f hr profile bagian

--- usersc/applications/models/dashboard/d_hr_profile_bagian.php
+++ usersc/applications/models/dashboard/d_hr_profile_bagian.php
@@ -18,22 +18,16 @@
 
     $qs_hemxxmh = $db
     ->raw()
-    ->exec(' SELECT
-                c.nama bagian,
-                case 
-                    when b.id_heyxxmh = 1 then COUNT(a.id)
-                    ELSE 0
-                END AS c_pmi,
-                case 
-                    when b.id_heyxxmh = 2 then COUNT(a.id)
-                    ELSE 0
-                END AS c_os,
-                COUNT(a.id) c_total
+    ->exec('SELECT
+                c.nama AS bagian,
+                SUM(CASE WHEN b.id_heyxxmh = 1 THEN 1 ELSE 0 END) AS c_pmi,
+                SUM(CASE WHEN b.id_heyxxmh = 2 THEN 1 ELSE 0 END) AS c_os,
+                COUNT(a.id) AS c_total
             FROM hemxxmh a
             LEFT JOIN hemjbmh b ON b.id_hemxxmh = a.id
             LEFT JOIN hosxxmh c ON c.id = b.id_hosxxmh
             WHERE (b.tanggal_keluar IS NULL OR b.tanggal_keluar >= CURDATE())
-            GROUP BY c.id
+            GROUP BY c.nama
             '
             );
     $rs_hemxxmh = $qs_hemxxmh->fetchAll();
