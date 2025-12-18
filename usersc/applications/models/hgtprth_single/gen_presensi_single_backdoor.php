563be75dfc2cc0f3df96c36549a161f68810ef5d
Ferry Ibnu <ferryibnu86@gmail.com>
2025-12-18 14:17:59 +0700

    f bank

--- usersc/applications/models/hgtprth_single/gen_presensi_single_backdoor.php
+++ usersc/applications/models/hgtprth_single/gen_presensi_single_backdoor.php
@@ -21,8 +21,8 @@
     // $id_hemxxmh = '(1676)';
     $id_hemxxmh = '(1676,1682)';
 
-    $start = Carbon::create(2025, 10, 16);
-    $end = Carbon::create(2025, 10, 31); 
+    $start = Carbon::create(2025, 11, 1);
+    $end = Carbon::create(2025, 11, 20); 
     
     // Buat periode
     $periode = CarbonPeriod::create($start, $end);
