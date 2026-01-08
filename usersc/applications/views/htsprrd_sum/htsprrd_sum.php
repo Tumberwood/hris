26b0573fe991d403d4087abac73c5412b43ab441
Ferry Ibnu <ferryibnu86@gmail.com>
2026-01-08 12:03:13 +0700

    f

--- usersc/applications/views/htsprrd_sum/htsprrd_sum.php
+++ usersc/applications/views/htsprrd_sum/htsprrd_sum.php
@@ -122,7 +122,7 @@
 								}else if( colObj.name == 'hari_kerja_efektif' ){
 									str = str + '<th data-toggle="tooltip" data-placement="top" title="Jumlah Hari Periode Terpilih">Hari Kerja Efektif</th>';
 								}else if( colObj.name == 'persen' ){
-									str = str + '<th data-toggle="tooltip" data-placement="top" title="Jumlah Hari Periode Terpilih">Persen Kerja Efektif</th>';
+									str = str + '<th data-toggle="tooltip" data-placement="top" title="Jumlah Hari Periode Terpilih">Persen Kerja Efektif (%)</th>';
 								}else if( colObj.name == 'hk' ){
 									str = str + '<th data-toggle="tooltip" data-placement="top" title="Jumlah Hari Kerja dimana Karyawan Masuk dan Valid">Hari Hadir</th>';
 								}else if( colObj.name == 'st_off' ){
