7271103fc1e4b1f437466019ad3d49866b292abe
Ferry Ibnu <ferryibnu86@gmail.com>
2026-01-12 17:24:14 +0700

    f

--- usersc/applications/models/hgtprth/hgtprth_fn_gen_presensi_ferry_v3.php
+++ usersc/applications/models/hgtprth/hgtprth_fn_gen_presensi_ferry_v3.php
@@ -1329,7 +1329,7 @@
 
                             FROM hitung_lembur
                         )
-                        SELECT
+                        SELECT DISTINCT
                             id_hemxxmh,
                             keterangan,
                             kode_finger,
