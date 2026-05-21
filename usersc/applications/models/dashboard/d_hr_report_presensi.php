9a1b01508b27fff373779b38ee3d7b966ac5e150
Ferry Ibnu <ferryibnu86@gmail.com>
2026-05-21 12:34:57 +0700

    f ceklok

--- usersc/applications/models/dashboard/d_hr_report_presensi.php
+++ usersc/applications/models/dashboard/d_hr_report_presensi.php
@@ -121,7 +121,13 @@
 					c.jam_akhir
 				FROM htsprrd AS a
 				LEFT JOIN htssctd AS b ON b.id_hemxxmh = a.id_hemxxmh AND b.tanggal = a.tanggal 
-				LEFT JOIN htoxxrd AS c ON c.id_hemxxmh = a.id_hemxxmh AND c.tanggal = a.tanggal
+				LEFT JOIN (
+					SELECT
+						*
+					FROM htoxxrd rd
+					WHERE rd.tanggal = :start_date AND rd.id_hemxxmh = :id_hemxxmh
+					GROUP BY rd.id_hemxxmh
+				) AS c ON c.id_hemxxmh = a.id_hemxxmh AND c.tanggal = a.tanggal
 				LEFT JOIN grup AS d ON d.id_hemxxmh = a.id_hemxxmh
 				LEFT JOIN htsxxmh AS f on f.id = b.id_htsxxmh
 				WHERE a.tanggal = :start_date AND a.id_hemxxmh = :id_hemxxmh AND b.is_active = 1 
