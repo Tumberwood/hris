888b2cae2a4ffb7b74e556f0620cd86dc1ba7d61
Ferry Ibnu <ferryibnu86@gmail.com>
2026-05-22 10:32:03 +0700

    f

--- usersc/applications/models/hgtprth/hgtprth_fn_gen_presensi_ferry_v3.php
+++ usersc/applications/models/hgtprth/hgtprth_fn_gen_presensi_ferry_v3.php
@@ -1441,8 +1441,7 @@
                                 CASE
                                     WHEN is_pot_premi_absen = 1 THEN 1
                                     
-                                    -- LATE 1 yang ada izin ini tidak dipotong premi
-                                    WHEN st_clock_in = "LATE 1" AND is_pot_premi_izin_in = 1 THEN 0
+                                    -- WHEN st_clock_in = "LATE 1" AND is_pot_premi_izin_in = 1 THEN 0
 
                                     -- Pak Rian 22 May 2026, LATE 1 dipotong premi (yg tidak ada izin)
                                     WHEN st_clock_in = "LATE 1" THEN 1
