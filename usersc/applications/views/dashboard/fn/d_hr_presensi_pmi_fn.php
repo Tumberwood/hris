d4eb477a1621b1650efcfa986eabaed60ebac911
Ferry Ibnu <ferryibnu86@gmail.com>
2026-05-26 09:56:07 +0700

    f

--- usersc/applications/views/dashboard/fn/d_hr_presensi_pmi_fn.php
+++ usersc/applications/views/dashboard/fn/d_hr_presensi_pmi_fn.php
@@ -295,7 +295,7 @@
                 // kalau table sudah ada → reset dulu
                 if ($.fn.dataTable.isDataTable('#tblhtlxxrh_absen')) {
                     $('#tblhtlxxrh_absen').DataTable().clear().destroy();
-                    $('#tblhtlxxrh_absen').empty();
+                    $('#tblhtlxxrh_absen tbody').empty();
                 }
 
                 // build DataTable baru
