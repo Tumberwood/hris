fb0fc656b83438b92ae9a5538a90ed5ae23c26c9
Ferry Ibnu <ferryibnu86@gmail.com>
2026-06-23 12:10:25 +0700

    f

--- usersc/applications/views/hpyxxth_cocokan/fn/hpyxxth_cocokan_fn.php
+++ usersc/applications/views/hpyxxth_cocokan/fn/hpyxxth_cocokan_fn.php
@@ -1,7 +1,9 @@
 <script>
     function compareField(row, data, index, field) {
         if ((data.hpyemtd_cocokan?.[field] ?? 0) != (data.hpyemtd?.[field] ?? 0)) {
-            $(row).find(`td:eq(${index})`).addClass('bg-danger');
+            $(row).find(`td:eq(${index})`)
+                .addClass('bg-danger')
+                .attr('style', 'background:#dc3545 !important;color:#fff !important;');
         }
     }
 </script>
