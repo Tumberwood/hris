bdd14b8a0b712542b8e548e6c948a8d673904d98
Ferry Ibnu <ferryibnu86@gmail.com>
2026-06-26 10:41:13 +0700

    f

--- usersc/applications/models/istirahat_ti_reguler/istirahat_ti_reguler.php
+++ usersc/applications/models/istirahat_ti_reguler/istirahat_ti_reguler.php
@@ -383,8 +383,7 @@
 				
 				WHERE 
 					a.tanggal BETWEEN :start_date AND :end_date
-					-- AND a.is_pot_premi <> 1 -- yang potongan jam karena early, late dsb ini agar tidak masuk
-					AND a.pot_jam_istirahat > 0
+					AND (a.is_pot_premi <> 1 OR AND a.pot_jam_istirahat > 0) -- yang potongan jam karena early, late dsb ini agar tidak masuk
 				--	AND a.durasi_lembur_total_jam = 0
 				-- AND a.pot_jam > 0
 					'.$where.'
