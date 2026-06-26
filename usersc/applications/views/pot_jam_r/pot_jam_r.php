eaa7aeb6e163e2443ccdcb5160d453f5b39b6be6
Ferry Ibnu <ferryibnu86@gmail.com>
2026-06-26 13:43:32 +0700

    f

--- usersc/applications/views/pot_jam_r/pot_jam_r.php
+++ usersc/applications/views/pot_jam_r/pot_jam_r.php
@@ -221,7 +221,7 @@
 						searchPanes:{
 							show: true,
 						},
-						targets: [3,4,5,6,7,8]
+						targets: [3,4,5,6,7,8,9]
 					},
 					{
 						searchPanes:{
--- usersc/applications/views/pot_upah_r/pot_upah_r.php
+++ usersc/applications/views/pot_upah_r/pot_upah_r.php
@@ -221,7 +221,7 @@
 						searchPanes:{
 							show: true,
 						},
-						targets: [1,3,4,5,6]
+						targets: [3,4,5,6,7,8,9]
 					},
 					{
 						searchPanes:{
