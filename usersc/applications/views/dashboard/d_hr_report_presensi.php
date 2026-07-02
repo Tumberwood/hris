19bfe50839ce8996ef230e803d015d4aacdb37a1
Ferry Ibnu <ferryibnu86@gmail.com>
2026-07-02 09:46:37 +0700

    f

--- usersc/applications/views/dashboard/d_hr_report_presensi.php
+++ usersc/applications/views/dashboard/d_hr_report_presensi.php
@@ -956,14 +956,22 @@
 
 					start_date = moment($('#start_date').val()).format('YYYY-MM-DD');
 					
-					notifyprogress = $.notify({
-						message: 'Processing ...</br> Jangan tutup halaman sampai notifikasi ini hilang!'
-					}, {
-						z_index: 9999,
-						allow_dismiss: false,
-						type: 'info',
-						delay: 0
+					Swal.fire({
+						title: 'Processing...',
+						html: 'Jangan tutup halaman sampai proses selesai',
+						allowOutsideClick: false,
+						allowEscapeKey: false,
+						showConfirmButton: false,
+						didOpen: () => {
+							Swal.showLoading();
+						}
 					});
+
+					notifyprogress = {
+						close: function(){
+							Swal.close();
+						}
+					};
 
 					generateTable(counter);
 					// addCeklok();
