9686d4b76e4c63c6d52628582b43eb0dc6d8e71b
Ferry Ibnu <ferryibnu86@gmail.com>
2026-08-06 08:51:46 +0700

    f

--- usersc/applications/views/hpyxxth_cocokan/hpyxxth_cocokan.php
+++ usersc/applications/views/hpyxxth_cocokan/hpyxxth_cocokan.php
@@ -1898,33 +1898,34 @@
 					{ data: "hpyemtd_cocokan.t_jab", render: $.fn.dataTable.render.number(',', '.', 0), class: "text-right" },
 					{ data: "hpyemtd.t_jab", render: $.fn.dataTable.render.number(',', '.', 0), class: "text-right" },
 					{ data: "hpyemtd_cocokan.terima_lain", render: $.fn.dataTable.render.number(',', '.', 0), class: "text-right" },
+					{ data: "terima_lain_baru", render: $.fn.dataTable.render.number(',', '.', 0), class: "text-right" },
 					// { data: "hpyemtd.terima_lain", render: $.fn.dataTable.render.number(',', '.', 0), class: "text-right" },
-					{
-						data: null,
-						class: "text-right",
-						render: function (data, type, row) {
-							// Helper fungsi untuk convert ke float & antisipasi nilai undefined/null/NaN
-							const num = v => (parseFloat(v) || 0);
+					// {
+					// 	data: null,
+					// 	class: "text-right",
+					// 	render: function (data, type, row) {
+					// 		// Helper fungsi untuk convert ke float & antisipasi nilai undefined/null/NaN
+					// 		const num = v => (parseFloat(v) || 0);
 
-							const premiAbsBaru        = num(row.hpyemtd?.premi_abs);
-							const lemburLama          = num(row.hpyemtd_cocokan?.total_rp_lembur);
-							const lemburBaru          = num(row.hpyemtd?.total_rp_lembur);
-							const potUpahLama         = num(row.hpyemtd_cocokan?.pot_upah);
-							const potJamBaru          = num(row.hpyemtd?.pot_jam);
-							const potLainBeforePphBaru = num(row.hpyemtd?.pot_lain_before_pph);
+					// 		const premiAbsBaru        = num(row.hpyemtd?.premi_abs);
+					// 		const lemburLama          = num(row.hpyemtd_cocokan?.total_rp_lembur);
+					// 		const lemburBaru          = num(row.hpyemtd?.total_rp_lembur);
+					// 		const potUpahLama         = num(row.hpyemtd_cocokan?.pot_upah);
+					// 		const potJamBaru          = num(row.hpyemtd?.pot_jam);
+					// 		const potLainBeforePphBaru = num(row.hpyemtd?.pot_lain_before_pph);
 
-							// Hitung sesuai rumus (termasuk pengurangan pot_lain_before_pph)
-							const terimaLainBaru = premiAbsBaru - lemburLama + lemburBaru + potUpahLama - potJamBaru - potLainBeforePphBaru;
+					// 		// Hitung sesuai rumus (termasuk pengurangan pot_lain_before_pph)
+					// 		const terimaLainBaru = premiAbsBaru - lemburLama + lemburBaru + potUpahLama - potJamBaru - potLainBeforePphBaru;
 
-							// Jika dipanggil untuk Sorting / Filtering DataTables, kembalikan angka murni
-							if (type === 'sort' || type === 'type') {
-								return terimaLainBaru;
-							}
+					// 		// Jika dipanggil untuk Sorting / Filtering DataTables, kembalikan angka murni
+					// 		if (type === 'sort' || type === 'type') {
+					// 			return terimaLainBaru;
+					// 		}
 
-							// Tampilan ke HTML dengan format ribuan
-							return $.fn.dataTable.render.number(',', '.', 0).display(terimaLainBaru);
-						}
-					},
+					// 		// Tampilan ke HTML dengan format ribuan
+					// 		return $.fn.dataTable.render.number(',', '.', 0).display(terimaLainBaru);
+					// 	}
+					// },
 					{ data: "hpyemtd_cocokan.var_cost", render: $.fn.dataTable.render.number(',', '.', 0), class: "text-right" },
 					{ data: "hpyemtd.var_cost", render: $.fn.dataTable.render.number(',', '.', 0), class: "text-right" },
 					{ data: "hpyemtd_cocokan.tj_khusus", render: $.fn.dataTable.render.number(',', '.', 0), class: "text-right" },
