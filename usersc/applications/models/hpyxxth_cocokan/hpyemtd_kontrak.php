a279c90d5eb54bb276d4b4d3543055a2d9a798d7
Ferry Ibnu <ferryibnu86@gmail.com>
2026-08-06 14:20:40 +0700

    f

--- usersc/applications/models/hpyxxth_cocokan/hpyemtd_kontrak.php
+++ usersc/applications/models/hpyxxth_cocokan/hpyemtd_kontrak.php
@@ -206,8 +206,8 @@
 
 				// TAMBAHAN GAJI
 				// Field::inst( 'hpyemtd.terima_lain' ),
-				// Field::inst( 'hpyemtd.premi_abs - hpyemtd_cocokan.total_rp_lembur + hpyemtd.total_rp_lembur + IF( IFNULL(hpyemtd.pot_upah, 0) - IFNULL(hpyemtd_cocokan.pot_upah, 0) = 0, 0, IFNULL(hpyemtd_cocokan.pot_upah, 0) ) - hpyemtd.pot_jam - hpyemtd.pot_lain_before_pph AS hpyemtd.terima_lain' ),
-				Field::inst( 'IF( IFNULL(hpyemtd.premi_abs, 0) - IFNULL(hpyemtd_cocokan.premi_abs, 0) = 0, 0, IFNULL(hpyemtd.premi_abs, 0) ) - hpyemtd_cocokan.total_rp_lembur + hpyemtd.total_rp_lembur + IF( hpyemtd.pot_upah - hpyemtd_cocokan.pot_upah = 0, 0, hpyemtd_cocokan.pot_upah ) - hpyemtd.pot_jam - hpyemtd.pot_lain_before_pph AS hpyemtd.terima_lain' ),
+				// Field::inst( 'hpyemtd.premi_abs - hpyemtd_cocokan.total_rp_lembur + hpyemtd.total_rp_lembur + hpyemtd_cocokan.pot_upah - hpyemtd.pot_jam - hpyemtd.pot_lain_before_pph AS hpyemtd.terima_lain' ),
+				Field::inst( 'IF( IFNULL(hpyemtd.premi_abs, 0) - IFNULL(hpyemtd_cocokan.premi_abs, 0) = 0, 0, IFNULL(hpyemtd.premi_abs, 0) ) - hpyemtd_cocokan.total_rp_lembur + hpyemtd.total_rp_lembur + hpyemtd_cocokan.pot_upah - hpyemtd.pot_jam - hpyemtd.pot_lain_before_pph AS hpyemtd.terima_lain' ),
 
 				Field::inst( 'hpyemtd.lembur15_final' ),
 				Field::inst( 'hpyemtd.lembur2_final' ),
