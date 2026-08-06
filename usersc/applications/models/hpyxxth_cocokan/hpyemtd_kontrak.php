56b6dbe34006828902c17cd16a6a194292d43105
Ferry Ibnu <ferryibnu86@gmail.com>
2026-08-06 08:54:08 +0700

    f

--- usersc/applications/models/hpyxxth_cocokan/hpyemtd_kontrak.php
+++ usersc/applications/models/hpyxxth_cocokan/hpyemtd_kontrak.php
@@ -97,7 +97,8 @@
 				Field::inst( 'hpyemtd_cocokan.npwp' ),
 
 				// TAMBAHAN GAJI
-				Field::inst( 'hpyemtd_cocokan.terima_lain' ),
+				// Field::inst( 'hpyemtd.terima_lain' ),
+				Field::inst( 'hpyemtd.premi_abs - hpyemtd_cocokan.total_rp_lembur + hpyemtd.total_rp_lembur + hpyemtd_cocokan.pot_upah - hpyemtd.pot_jam - hpyemtd.pot_lain_before_pph AS terima_lain_baru' ),
 
 				Field::inst( 'hpyemtd_cocokan.lembur15_final' ),
 				Field::inst( 'hpyemtd_cocokan.lembur2_final' ),
