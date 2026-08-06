0f0ac26fd3f3d20a74a4f5137d1dade19f4c44c7
Ferry Ibnu <ferryibnu86@gmail.com>
2026-08-06 14:58:58 +0700

    f

--- usersc/applications/models/hgsptth_v3/hgsptth_v3_fn_gen_jadwal_ferry.php
+++ usersc/applications/models/hgsptth_v3/hgsptth_v3_fn_gen_jadwal_ferry.php
@@ -76,7 +76,7 @@
             ->bind(':tanggal_awal', $tanggal_awal)
             ->bind(':tanggal_akhir', $tanggal_akhir)
             ->exec('UPDATE htssctd AS b
-                    LEFT JOIN hgsemtd_v3 AS a ON a.id_hemxxmh = b.id_hemxxmh
+                    LEFT JOIN hgsemtd_v3 AS a ON a.id_hemxxmh = b.id_hemxxmh AND a.id_hgsptth_v3 = :id_hgsptth_v3
                     SET 
                         b.is_active = 0
                     WHERE tanggal BETWEEN :tanggal_awal AND :tanggal_akhir AND a.id_hgsptth_v3 = :id_hgsptth_v3
