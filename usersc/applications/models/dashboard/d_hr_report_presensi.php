29998fb957b3ff678b9ef5593bb2f0afc52cebe0
Ferry Ibnu <ferryibnu86@gmail.com>
2026-08-18 10:41:11 +0700

    f

--- usersc/applications/models/dashboard/d_hr_report_presensi.php
+++ usersc/applications/models/dashboard/d_hr_report_presensi.php
@@ -93,7 +93,7 @@
 					IF(a.st_jadwal = "OFF", 0, 
 						IF(TIMESTAMPDIFF(HOUR, b.tanggaljam_awal, b.tanggaljam_akhir) = 5, 
 							5, 
-							GREATEST(0, TIMESTAMPDIFF(HOUR, b.tanggaljam_awal, b.tanggaljam_akhir) - 1)
+							GREATEST(0, TIMESTAMPDIFF(HOUR, b.tanggaljam_awal, b.tanggaljam_akhir) - IF(a.jumlah_grup = 4, 0, 1))
 						)
 					) AS jam_wajib,
 
@@ -101,8 +101,8 @@
 					IF(a.st_jadwal = "OFF", 0, 
 						IF(TIMESTAMPDIFF(HOUR, b.tanggaljam_awal, b.tanggaljam_akhir) = 5, 
 							5, 
-							GREATEST(0, TIMESTAMPDIFF(HOUR, b.tanggaljam_awal, b.tanggaljam_akhir) - 1)
-						) - IFNULL(a.pot_hk,0)
+							GREATEST(0, TIMESTAMPDIFF(HOUR, b.tanggaljam_awal, b.tanggaljam_akhir) - IF(a.jumlah_grup = 4, 0, 1))
+						) - IFNULL(a.pot_hk, 0)
 					) AS jam_kerja,
 
 					-- IF(TIMESTAMPDIFF(HOUR, b.tanggaljam_awal, b.tanggaljam_akhir) = 5, 
