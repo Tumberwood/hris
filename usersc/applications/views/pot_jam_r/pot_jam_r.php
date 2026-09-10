ba2b89f2e56c9e3e54a567bb6d9637ac027c8c1f
Ferry Ibnu <ferryibnu86@gmail.com>
2026-09-10 09:31:44 +0700

    f

--- usersc/applications/views/pot_jam_r/pot_jam_r.php
+++ usersc/applications/views/pot_jam_r/pot_jam_r.php
@@ -88,9 +88,9 @@
 						<tfoot>
 							<tr>
 								<th colspan="2" class="text-right">Jumlah Orang</th>
-								<th class="text-right" id="c_orang">Jumlah Orang</th>
+								<th class="text-right fw-bold" id="c_orang">Jumlah Orang</th>
 								<th colspan="7" class="text-right">Grand Total</th>
-								<th class="text-right" id="grand_total_ip"></th>
+								<th class="text-right fw-bold" id="grand_total_ip"></th>
 							</tr>
 						</tfoot>
                     </table>
