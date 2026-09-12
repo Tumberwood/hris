63761eb5cb54cb2cf75e94552b8af9ece874de26
Ferry Ibnu <ferryibnu86@gmail.com>
2026-09-12 08:40:24 +0700

    f

--- usersc/applications/models/istirahat_4grup/istirahat_4grup.php
+++ usersc/applications/models/istirahat_4grup/istirahat_4grup.php
@@ -58,7 +58,7 @@
 						WHEN TIMESTAMPDIFF(MINUTE, a.break_in, a.break_out) > 0 AND IFNULL(is_makan, 0) = 1 THEN "Istirahat + Makan"
 						WHEN TIMESTAMPDIFF(MINUTE, a.break_in, a.break_out) > 30 THEN "Istirahat > 30 menit"
 						WHEN TIMESTAMPDIFF(MINUTE, a.break_in, a.break_out) <= 30 AND IFNULL(is_makan, 0) = 1 THEN "Istirahat ≤ 30 + Makan"
-						WHEN pot_jam_istirahat > 0 THEN "Jam Kerja Kurang 8 Jam"
+						WHEN pot_jam_istirahat > 0 THEN "durasi kerja < 7/8 jam"
 						ELSE "Tidak Masuk Kategori"
 					END AS kategori,
 					a.durasi_lembur_total_jam,
