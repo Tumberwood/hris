8f95f45cc2bd799b70bddb68420fa1c07f5c891d
Ferry Ibnu <ferryibnu86@gmail.com>
2026-09-16 10:25:23 +0700

    f

--- usersc/applications/views/hpyxxth_cocokan/hpyxxth_cocokan.php
+++ usersc/applications/views/hpyxxth_cocokan/hpyxxth_cocokan.php
@@ -1432,20 +1432,12 @@
 							
 							<div role="tabpanel" id="tabpentunjuk" class="tab-pane">
 								<div class="panel-body">
-									<div class="alert alert-info shadow-sm p-3 mb-3 border">
-    <div class="fw-bold mb-2">📌 Petunjuk Rumus Payroll:</div>
-    <div class="font-monospace text-dark mb-2">
-        <b>Terima Lain (Baru)</b> = 
-        <span class="text-primary">(Premi Absensi Lama - Baru == 0 ? 0 : Premi Absensi Baru)</span> - 
-        <span class="text-danger">Lembur Total Rp (Lama)</span> + 
-        <span class="text-success">Lembur Total Rp (Baru)</span> + 
-        <span class="text-success">(Potongan Upah Lama - Baru &lt; 2 ? 0 : Potongan Upah Lama)</span> - 
-        <span class="text-danger">Potongan Jam Rp (Baru)</span> - 
-        <span class="text-danger">Potongan Sebelum PPh 21 (Baru)</span>
-    </div>
-    <small class="text-muted">*Catatan: jika Premi Absensi (Lama) - Premi Absensi (Baru) = 0, maka tidak ditambahkan		.</small>
-	<br>
-    <small class="text-muted">*Catatan: jika Potongan Upah (Rp) (Lama) - Potongan Upah (Rp) (baru) < 2 maka tidak ditambahkan		.</small>
+									<div class="alert alert-info">
+    <b>📌 Petunjuk Rumus Payroll:</b><br>
+    <b>Terima Lain (Baru)</b> = Premi Absensi (Baru) - Lembur Total (rp) (Lama) + Lembur Total (rp) (Baru) + Potongan Upah (Rp) (Lama) - Potongan Jam (Rp) (Baru) - Potongan Sebelum PPh 21 (Baru)
+    <br><br>
+    <small>* Catatan 1: jika Premi Absensi (Lama) - Premi Absensi (Baru) = 0, maka Premi Absensi (Baru) tidak ditambahkan.</small><br>
+    <small>* Catatan 2: jika Potongan Upah (Rp) (Lama) - Potongan Upah (Rp) (Baru) &lt; 2, maka Potongan Upah (Rp) (Lama) tidak ditambahkan.</small>
 </div>
 								</div>
 							</div>
