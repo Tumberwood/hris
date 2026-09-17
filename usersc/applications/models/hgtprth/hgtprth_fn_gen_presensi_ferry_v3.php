0116975f2040a528b2996fc5e9092fe79649ae47
Ferry Ibnu <ferryibnu86@gmail.com>
2026-09-17 12:31:12 +0700

    f

--- usersc/applications/models/hgtprth/hgtprth_fn_gen_presensi_ferry_v3.php
+++ usersc/applications/models/hgtprth/hgtprth_fn_gen_presensi_ferry_v3.php
@@ -708,6 +708,18 @@
                                                         )
                                                     )
                                                 THEN CONCAT(c.tanggal_jam,"|",c.nama)
+                                                
+                                                -- CEK untuk ceklok mesin istirahat yang diluar range istirahat jadwal
+                                                WHEN c.nama IN ("istirahat")
+                                                    AND c.tanggal_jam BETWEEN
+                                                        jadwal.tanggaljam_awal_t1
+                                                        AND jadwal.tanggaljam_akhir_t2
+                                                        
+                                                    AND c.tanggal_jam NOT BETWEEN
+                                                        jadwal.tanggaljam_awal_istirahat
+                                                        AND jadwal.tanggaljam_akhir_istirahat
+                                                THEN CONCAT(c.tanggal_jam,"|",c.nama)
+                                                
                                             END
                                         ) AS concat_break_in,
                                         
@@ -853,6 +865,18 @@
                                                         )
                                                     )
                                                 THEN CONCAT(c.tanggal_jam,"|",c.nama)
+
+                                                -- CEK untuk ceklok mesin istirahat yang diluar range istirahat jadwal
+                                                WHEN c.nama IN ("istirahat")
+                                                    AND c.tanggal_jam BETWEEN
+                                                        jadwal.tanggaljam_awal_t1
+                                                        AND jadwal.tanggaljam_akhir_t2
+                                                        
+                                                    AND c.tanggal_jam NOT BETWEEN
+                                                        jadwal.tanggaljam_awal_istirahat
+                                                        AND jadwal.tanggaljam_akhir_istirahat
+                                                THEN CONCAT(c.tanggal_jam,"|",c.nama)
+
                                             END
                                         ) AS concat_break_out,
                                         
