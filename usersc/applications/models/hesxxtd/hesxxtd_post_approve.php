123a6e170d922043cd6ea4068e37ae567d61ad61
Ferry Ibnu <ferryibnu86@gmail.com>
2026-09-18 15:43:35 +0700

    f

--- usersc/applications/models/hesxxtd/hesxxtd_post_approve.php
+++ usersc/applications/models/hesxxtd/hesxxtd_post_approve.php
@@ -410,7 +410,7 @@
 			// 	}
 			// }
 
-			if ($keputusan == 'Tetap' || $keputusan == 'Kontrak') {
+			if ($keputusan == 'Tetap') {
 				$qu_hemxxmh = $db
 					->query('update', 'hemjbmh')
 					->set('tanggal_keluar', $tanggal_mulai)
