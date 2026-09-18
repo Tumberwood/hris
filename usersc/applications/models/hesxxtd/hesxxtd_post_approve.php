b865884e9c3e4ace22c0d96a9f1cc13c2b2da87e
Ferry Ibnu <ferryibnu86@gmail.com>
2026-09-18 15:36:09 +0700

    f

--- usersc/applications/models/hesxxtd/hesxxtd_post_approve.php
+++ usersc/applications/models/hesxxtd/hesxxtd_post_approve.php
@@ -410,7 +410,7 @@
 			// 	}
 			// }
 
-			if ($keputusan == 'Tetap' || $keputusan == 'Kontrak' || $keputusan == 'Rekontrak') {
+			if ($keputusan == 'Tetap' || $keputusan == 'Kontrak') {
 				$qu_hemxxmh = $db
 					->query('update', 'hemjbmh')
 					->set('tanggal_keluar', $tanggal_mulai)
