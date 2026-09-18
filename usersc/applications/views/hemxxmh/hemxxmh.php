cb4af28de5f3e474a0a416bf88c11ff239756882
Ferry Ibnu <ferryibnu86@gmail.com>
2026-09-18 10:12:24 +0700

    f

--- usersc/applications/views/hemxxmh/hemxxmh.php
+++ usersc/applications/views/hemxxmh/hemxxmh.php
@@ -1500,23 +1500,23 @@
 					}
 					
 					//Validasi nama unik
-					$.ajax( {
-						url: '../../../helpers/validate_fn_unique.php',
-						dataType: 'json',
-						type: 'POST',
-						async: false,
-						data: {
-							table_name: 'hemxxmh',
-							nama_field: 'nama',
-							nama_field_value: '"'+nama+'"',
-							id_transaksi: id_hemxxmh
-						},
-						success: function ( json ) {
-							if(json.data.count == 1){
-								edthemxxmh.field('hemxxmh.nama').error( 'Data tidak boleh kembar!' );
-							}
-						}
-					} );
+					// $.ajax( {
+					// 	url: '../../../helpers/validate_fn_unique.php',
+					// 	dataType: 'json',
+					// 	type: 'POST',
+					// 	async: false,
+					// 	data: {
+					// 		table_name: 'hemxxmh',
+					// 		nama_field: 'nama',
+					// 		nama_field_value: '"'+nama+'"',
+					// 		id_transaksi: id_hemxxmh
+					// 	},
+					// 	success: function ( json ) {
+					// 		if(json.data.count == 1){
+					// 			edthemxxmh.field('hemxxmh.nama').error( 'Data tidak boleh kembar!' );
+					// 		}
+					// 	}
+					// } );
 					// END of validasi hemxxmh.nama 
 					
 					// BEGIN of validasi hemxxmh.is_pot_makan 
